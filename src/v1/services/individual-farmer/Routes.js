49a60d55d66863a555f4aef0664592093c59c9c9
Maneesh Kumar Yadav <maneesh@radiantinfonet.com>
2024-09-04 12:00:59 +0530

    update routes

--- src/v1/services/individual-farmer/Routes.js
+++ src/v1/services/individual-farmer/Routes.js
@@ -1,10 +1,5 @@
 const express = require("express");
 const individualFarmerRoutes = express.Router();
-const { body } = require("express-validator");
-const { getVariety, getVarietyById, createVariety, updateVariety, getUnit, getUnitById, createUnit, updateUnit, deleteUnit, deleteVariety, getGrade, getGradeById, createGrade, updateGrade, deleteGrade, getOrganizations, updateOrganization, createOrganization } = require("./Controller");
-const { _middleware } = require("@src/v1/utils/constants/messages");
-const { validateErrors } = require("@src/v1/utils/helpers/express_validator");
-const { _status } = require("@src/v1/utils/constants/index");
 const {saveFarmerDetails} =require('./Controller')
 const { verifyJwtToken,generateJwtToken } = require("@src/v1/utils/helpers/jwt");
 //console.log(generateJwtToken({mobile_no:"8864995639"}))
