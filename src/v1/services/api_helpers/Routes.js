a44a48ec9b9090be49e5690374b4273123870b5d
LAP-RIPL068\Admin <ayushtiwari653@gmail.com>
2024-09-04 11:48:47 +0530

    pincode addrsss third party

---
+++ src/v1/services/api_helpers/Routes.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const helperRoutes = express.Router()
+const { getExcelTemplate, getAddressByPincode } = require("./Controller");
+const { validateErrors } = require("@src/v1/utils/helpers/express_validator");
+const { query } = require("express-validator");
+const { _middleware } = require("@src/v1/utils/constants/messages");
+
+helperRoutes.get('/templete', [
+    query('template_name', _middleware.require("template_name")).notEmpty().trim(),
+], validateErrors, getExcelTemplate)
+helperRoutes.get('/address', [
+    query('pincode', _middleware.require("pincode field must be a 6-digit number")).notEmpty().trim().matches(/^\d{6}$/),
+], validateErrors, getAddressByPincode)
+
+module.exports = { helperRoutes }
