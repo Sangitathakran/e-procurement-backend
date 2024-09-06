abae1fd1ee2805904def3a3cd8f0c3f37987465d
Sangita <sangita@navankur.org>
2024-09-06 12:50:53 +0530

    https://radiantinfonet.atlassian.net/browse/NV-116

--- src/v1/services/procurement/order/Controller.js
+++ src/v1/services/procurement/order/Controller.js
@@ -120,3 +120,38 @@ module.exports.viewTrackDelivery = async (req, res) => {
         _handleCatchErrors(error, res);
     }
 }
+
+module.exports.trackDeliveryByBatchId = async (req, res) => {
+
+    try {
+        const { page, limit, skip, paginate = 1, sortBy, search = '' } = req.query
+        const { batch_id } = req
+        let query = {
+            batch_id: batch_id,
+            ...(search ? { name: { $regex: search, $options: "i" } ,deletedAt: null} : { deletedAt: null })
+        };
+        const records = { count: 0 };
+        records.rows = paginate == 1 ? await AssociateOrders.find(query)
+            .sort(sortBy)
+            .skip(skip)
+            .limit(parseInt(limit)) : await AssociateOrders.find(query)
+            .select('dispatched intransit status')
+            .populate({ 
+                path:'req_id', select:'product address'
+             })
+            .sort(sortBy);           
+
+        records.count = await AssociateOrders.countDocuments(query);
+
+        if (paginate == 1) {
+            records.page = page
+            records.limit = limit
+            records.pages = limit != 0 ? Math.ceil(records.count / limit) : 0
+        }
+
+        return res.status(200).send(new serviceResponse({ status: 200, data: records, message: _response_message.found("Track order") }));
+
+    } catch (error) {
+        _handleCatchErrors(error, res);
+    }
+}
--- src/v1/services/procurement/order/Routes.js
+++ src/v1/services/procurement/order/Routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { associateOrder, viewTrackDelivery } = require("./Controller");
+const { associateOrder, viewTrackDelivery, trackDeliveryByBatchId } = require("./Controller");
 const { verifyJwtToken } = require("@src/v1/utils/helpers/jwt");
 
 const orderRoutes = express.Router();
 
 orderRoutes.post("/associate-order", verifyJwtToken, associateOrder);
-orderRoutes.get("/view-TrackDelivery", verifyJwtToken, viewTrackDelivery);
-
+orderRoutes.get("/view-trackDelivery", verifyJwtToken, viewTrackDelivery);
+orderRoutes.get("/trackDelivery-by-batchId", verifyJwtToken, trackDeliveryByBatchId);
 
 module.exports = { orderRoutes }; 
