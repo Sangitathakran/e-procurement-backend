c250e0cce023ddf466f7f9eaa5e997b5b2c06512
Ankush <ankush@navankur.org>
2024-09-11 11:49:31 +0530

    complete import functionality of procurement centers

--- src/v1/services/procurement/collection_center/Controller.js
+++ src/v1/services/procurement/collection_center/Controller.js
@@ -1,7 +1,8 @@
 const { _handleCatchErrors } = require("@src/v1/utils/helpers")
 const { serviceResponse } = require("@src/v1/utils/helpers/api_response");
-const {  _response_message } = require("@src/v1/utils/constants/messages");
+const {  _response_message, _middleware } = require("@src/v1/utils/constants/messages");
 const { CollectionCenter } = require("@src/v1/models/app/procurement/CollectionCenter");
+const { decryptJwtToken } = require("@src/v1/utils/helpers/jwt");
 const xlsx = require('xlsx');
 const csv = require("csv-parser");
 const Readable = require('stream').Readable;
@@ -60,92 +61,92 @@ module.exports.getCollectionCenter = async (req, res) => {
 
 module.exports.ImportCollectionCenter = async (req, res) => {
     try {
-        
         const { isxlsx = 1 } = req.body;
         const [file] = req.files;
-        
+
         if (!file) {
-            return res.status(400).json({
-                message: _response_message.notFound("file"),
-                status: 400
-            });
+            return res.status(400).send(new serviceResponse({ status: 400, message: _response_message.notFound('file') }));
         }
 
         let centers = [];
         let headers = [];
-        
+
+        // Process XLSX or CSV file
         if (isxlsx) {
             const workbook = xlsx.read(file.buffer, { type: 'buffer' });
-            const sheetName = workbook.SheetNames[0];
-            const worksheet = workbook.Sheets[sheetName];
+            const worksheet = workbook.Sheets[workbook.SheetNames[0]];
             centers = xlsx.utils.sheet_to_json(worksheet);
-            console.log('centers',centers);return false;
+
+            if (!centers.length) {
+                return res.status(400).send(new serviceResponse({ status: 400, message: _response_message.notFound('No data found in the file') }));
+            }
+
             headers = Object.keys(centers[0]);
-            
+
         } else {
             const csvContent = file.buffer.toString('utf8');
             const lines = csvContent.split('\n');
             headers = lines[0].trim().split(',');
-            const dataContent = lines.slice(1).join('\n');
 
+            const dataContent = lines.slice(1).join('\n');
             const parser = csv({ headers });
             const readableStream = Readable.from(dataContent);
 
             readableStream.pipe(parser);
-            parser.on('data', async (data) => {
-                if (Object.values(data).some(val => val !== '')) {
-                    const result = await processCenterRecord(data);
-                    if (!result.success) {
-                        errorArray = errorArray.concat(result.errors);
-                    }
-                }
-            });
 
-            parser.on('end', (err, data) => {
-                console.log("Streem end")
-            });
-            parser.on('error', (err, data) => {
-                console.log("Streem error")
+            await new Promise((resolve, reject) => {
+                parser.on('data', async (data) => {
+                    if (Object.values(data).some(val => val.trim() !== '')) {
+                        const result = await processCenterRecord(data);
+                        if (!result.success) errorArray = errorArray.concat(result.errors);
+                    }
+                });
+
+                parser.on('end', resolve);
+                parser.on('error', reject);
             });
         }
-        console.log('test route');return false;
+
+        const getToken = req.headers.token || req.cookies.token;
+        if (!getToken) {
+            return res.status(401).send(new serviceResponse({ status: 401, message: _middleware.require('token') }));
+        }
+
+        const decode = await decryptJwtToken(getToken);
+        const userId = decode.data.user_id;
+
         let errorArray = [];
+
         const processCenterRecord = async (rec) => {
-            const agencyId = rec["Agency Id*"];
-            const user_id = rec["User Id"];
-            const line1 = rec["line1*"];
-            const line2 = rec["line2*"];
-            const country = rec["country"];
-            const state = rec["state*"];
-            const district = rec["district*"];
-            const city = rec["city"];
-            const postalCode = rec["postalCode"];
-            const name = rec["name"];
-            const email = rec["email"];
-            const mobile = rec["mobile"];
-            const designation = rec["designation"];
-            const aadhar_no = rec["aadhar_no*"];
-            
+            const requiredFields = ['line1', 'line2', 'country', 'state', 'district', 'city', 'postalCode', 'name', 'email', 'mobile', 'designation', 'aadhar_number'];
+            const missingFields = requiredFields.filter(field => !rec[field]);
+
             let errors = [];
 
-            if (!line1 || !line2 || !country || !state || !district || !city || !postalCode || !name || !email || !mobile || !designation || !aadhar_no) {
-                errors.push({ record: rec, error: "Required fields missing" });
+            if (missingFields.length) {
+                errors.push({ record: rec, error: `Required fields missing: ${missingFields.join(', ')}` });
             }
-            if (!/^\d{12}$/.test(aadhar_no)) {
+
+            if (!/^\d{12}$/.test(rec.aadhar_number)) {
                 errors.push({ record: rec, error: "Invalid Aadhar Number" });
             }
-            if (!/^\d{10}$/.test(mobile)) {
+            if (!/^\d{10}$/.test(rec.mobile)) {
                 errors.push({ record: rec, error: "Invalid Mobile Number" });
             }
 
             if (errors.length > 0) return { success: false, errors };
 
             try {
-                await insertNewCenterRecord({
-                    agencyId, user_id, line1, line2, country, state_id, district_id, city, postalCode, name, email, mobile, designation
+                await CollectionCenter.create({
+                    agencyId: 1223,
+                    user_id: userId,
+                    address: {
+                        line1: rec.line1, line2: rec.line2, country: rec.country, state: rec.state, district: rec.district, city: rec.city, postalCode: rec.postalCode
+                    },
+                    point_of_contact: {
+                        name: rec.name, email: rec.email, mobile: rec.mobile, designation: rec.designation, aadhar_number: rec.aadhar_number, aadhar_image: 'aa'
+                    }
                 });
-                
-
             } catch (error) {
                 errors.push({ record: rec, error: error.message });
             }
@@ -161,19 +162,11 @@ module.exports.ImportCollectionCenter = async (req, res) => {
         }
 
         if (errorArray.length > 0) {
-            return res.status(200).json({
-                status: 400,
-                data: { records: errorArray },
-                errors: [{ message: "Partial upload successful. Please check the error records." }]
-            });
+            return res.status(200).send(new serviceResponse({ status: 400, data: { records: errorArray }, errors: [{ message: "Partial upload successfull ! Please export to view the uploaded data." }] }))
         } else {
-            return res.status(200).json({
-                status: 200,
-                data: {},
-                message: "centers successfully uploaded."
-            });
+            return res.status(200).send(new serviceResponse({ status: 200, data: {}, message: 'Centers successfully uploaded.' }))
         }
-
+        
     } catch (error) {
         _handleCatchErrors(error, res);
     }
