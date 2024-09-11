04c13112f4992f001ac6f1bac2dc8023c3657129
Sangita <sangita@navankur.org>
2024-09-11 17:07:17 +0530

    https://radiantinfonet.atlassian.net/browse/NV-124

--- src/v1/services/procurement/payment/Controller.js
+++ src/v1/services/procurement/payment/Controller.js
@@ -8,10 +8,75 @@ const { Payment } = require("@src/v1/models/app/procurement/Payment");
 module.exports.payment = async (req, res) => {
 
     try {
-        const { page, limit, skip, paginate = 1, sortBy, search = '', req_id } = req.query
-
+        const { page, limit, skip, paginate = 1, sortBy, search = '' } = req.query
+     
         let query = {
-            req_id,
+            ...(search ? { name: { $regex: search, $options: "i" } } : {})
+        };
+        const records = { count: 0 };
+        records.rows = paginate == 1 ? await Payment.find(query)
+            .populate({
+                path: 'req_id', select: 'product address'
+            })
+            .sort(sortBy)
+            .skip(skip)
+            .limit(parseInt(limit)) : await Payment.find(query).sort(sortBy);
+
+        records.count = await Payment.countDocuments(query);
+
+        if (paginate == 1) {
+            records.page = page
+            records.limit = limit
+            records.pages = limit != 0 ? Math.ceil(records.count / limit) : 0
+        }
+
+        return res.status(200).send(new serviceResponse({ status: 200, data: records, message: _response_message.found("Payment") }));
+
+    } catch (error) {
+        _handleCatchErrors(error, res);
+    }
+}
+
+module.exports.farmentPayment = async (req, res) => {
+
+    try {
+        const { page, limit, skip, paginate = 1, sortBy, search = '' } = req.query
+        const {user_type } = req;
+        let query = {
+            user_type:'farmer',
+            ...(search ? { name: { $regex: search, $options: "i" } } : {})
+        };
+        const records = { count: 0 };
+        records.rows = paginate == 1 ? await Payment.find(query)
+            .populate({
+                path: 'req_id', select: 'product address'
+            })
+            .sort(sortBy)
+            .skip(skip)
+            .limit(parseInt(limit)) : await Payment.find(query).sort(sortBy);
+
+        records.count = await Payment.countDocuments(query);
+
+        if (paginate == 1) {
+            records.page = page
+            records.limit = limit
+            records.pages = limit != 0 ? Math.ceil(records.count / limit) : 0
+        }
+
+        return res.status(200).send(new serviceResponse({ status: 200, data: records, message: _response_message.found("Payment") }));
+
+    } catch (error) {
+        _handleCatchErrors(error, res);
+    }
+}
+
+module.exports.associatePayment = async (req, res) => {
+
+    try {
+        const { page, limit, skip, paginate = 1, sortBy, search = '' } = req.query
+        const {user_type } = req;
+        let query = {
+            user_type:'associate',
             ...(search ? { name: { $regex: search, $options: "i" } } : {})
         };
         const records = { count: 0 };
--- src/v1/services/procurement/payment/Routes.js
+++ src/v1/services/procurement/payment/Routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { payment } = require("./Controller");
+const { payment, farmentPayment, associatePayment } = require("./Controller");
 const { verifyJwtToken } = require("@src/v1/utils/helpers/jwt");
 
 const paymentRoutes = express.Router();
 
 paymentRoutes.get("/", verifyJwtToken, payment);
+paymentRoutes.get("/farmentPayment", verifyJwtToken, farmentPayment);
+paymentRoutes.get("/associatePayment", verifyJwtToken, associatePayment);
 
 module.exports = { paymentRoutes }; 
