3469321031479abf3e0fc95b4a0cd4566250822c
Ankush <ankush@navankur.org>
2024-09-11 09:27:06 +0530

    work on import functionality of centers

--- src/v1/services/procurement/collection_center/Controller.js
+++ src/v1/services/procurement/collection_center/Controller.js
@@ -2,6 +2,9 @@ const { _handleCatchErrors } = require("@src/v1/utils/helpers")
 const { serviceResponse } = require("@src/v1/utils/helpers/api_response");
 const {  _response_message } = require("@src/v1/utils/constants/messages");
 const { CollectionCenter } = require("@src/v1/models/app/procurement/CollectionCenter");
+const xlsx = require('xlsx');
+const csv = require("csv-parser");
+const Readable = require('stream').Readable;
 
 module.exports.createCollectionCenter = async (req, res) => {
 
@@ -54,3 +57,124 @@ module.exports.getCollectionCenter = async (req, res) => {
         _handleCatchErrors(error, res);
     }
 }
+
+module.exports.ImportCollectionCenter = async (req, res) => {
+    try {
+        
+        const { isxlsx = 1 } = req.body;
+        const [file] = req.files;
+        
+        if (!file) {
+            return res.status(400).json({
+                message: _response_message.notFound("file"),
+                status: 400
+            });
+        }
+
+        let centers = [];
+        let headers = [];
+        
+        if (isxlsx) {
+            const workbook = xlsx.read(file.buffer, { type: 'buffer' });
+            const sheetName = workbook.SheetNames[0];
+            const worksheet = workbook.Sheets[sheetName];
+            centers = xlsx.utils.sheet_to_json(worksheet);
+            console.log('centers',centers);return false;
+            headers = Object.keys(centers[0]);
+            
+        } else {
+            const csvContent = file.buffer.toString('utf8');
+            const lines = csvContent.split('\n');
+            headers = lines[0].trim().split(',');
+            const dataContent = lines.slice(1).join('\n');
+
+            const parser = csv({ headers });
+            const readableStream = Readable.from(dataContent);
+
+            readableStream.pipe(parser);
+            parser.on('data', async (data) => {
+                if (Object.values(data).some(val => val !== '')) {
+                    const result = await processCenterRecord(data);
+                    if (!result.success) {
+                        errorArray = errorArray.concat(result.errors);
+                    }
+                }
+            });
+
+            parser.on('end', (err, data) => {
+                console.log("Streem end")
+            });
+            parser.on('error', (err, data) => {
+                console.log("Streem error")
+            });
+        }
+        console.log('test route');return false;
+        let errorArray = [];
+        const processCenterRecord = async (rec) => {
+            const agencyId = rec["Agency Id*"];
+            const user_id = rec["User Id"];
+            const line1 = rec["line1*"];
+            const line2 = rec["line2*"];
+            const country = rec["country"];
+            const state = rec["state*"];
+            const district = rec["district*"];
+            const city = rec["city"];
+            const postalCode = rec["postalCode"];
+            const name = rec["name"];
+            const email = rec["email"];
+            const mobile = rec["mobile"];
+            const designation = rec["designation"];
+            const aadhar_no = rec["aadhar_no*"];
+            
+            let errors = [];
+
+            if (!line1 || !line2 || !country || !state || !district || !city || !postalCode || !name || !email || !mobile || !designation || !aadhar_no) {
+                errors.push({ record: rec, error: "Required fields missing" });
+            }
+            if (!/^\d{12}$/.test(aadhar_no)) {
+                errors.push({ record: rec, error: "Invalid Aadhar Number" });
+            }
+            if (!/^\d{10}$/.test(mobile)) {
+                errors.push({ record: rec, error: "Invalid Mobile Number" });
+            }
+
+            if (errors.length > 0) return { success: false, errors };
+
+            try {
+                await insertNewCenterRecord({
+                    agencyId, user_id, line1, line2, country, state_id, district_id, city, postalCode, name, email, mobile, designation
+                });
+                
+
+            } catch (error) {
+                errors.push({ record: rec, error: error.message });
+            }
+
+            return { success: errors.length === 0, errors };
+        };
+
+        for (const center of centers) {
+            const result = await processCenterRecord(center);
+            if (!result.success) {
+                errorArray = errorArray.concat(result.errors);
+            }
+        }
+
+        if (errorArray.length > 0) {
+            return res.status(200).json({
+                status: 400,
+                data: { records: errorArray },
+                errors: [{ message: "Partial upload successful. Please check the error records." }]
+            });
+        } else {
+            return res.status(200).json({
+                status: 200,
+                data: {},
+                message: "centers successfully uploaded."
+            });
+        }
+
+    } catch (error) {
+        _handleCatchErrors(error, res);
+    }
+};
