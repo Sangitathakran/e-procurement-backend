9ebd6a18367267efb4be02c59758d1662d43e161
Maneesh Kumar Yadav <maneesh@radiantinfonet.com>
2024-09-12 18:48:48 +0530

    update ho date

--- src/v1/services/ho-dashboard/Routes.js
+++ src/v1/services/ho-dashboard/Routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const hoDashboardRoutes = express.Router();
 
-const {widgetList } = require("./Controller");
+const { widgetList } = require("./Controller");
 
-hoDashboardRoutes.get('/widget-list',widgetList);
+hoDashboardRoutes.get("/widget-list", widgetList);
 
 module.exports = { hoDashboardRoutes };
