fb1980e8347ca2762c0608e061c1812c9418111c
LAP-RIPL068\Admin <ayushtiwari653@gmail.com>
2024-09-19 15:28:27 +0530

    agent create req fix

--- src/v1/modules/agent/request/Controller.js
+++ src/v1/modules/agent/request/Controller.js
@@ -11,17 +11,25 @@ const { AssociateOffers } = require("@src/v1/models/app/procurement/AssociateOff
 
 module.exports.createProcurement = asyncErrorHandler(async (req, res) => {
     const { user_id, user_type } = req
-    const { organization_id, quotedPrice, deliveryDate, name, commodityImage, grade, variety, quantity, deliveryLocation, lat, long, quoteExpiry, head_office_id, branch_id, expectedProcurementDate } = req.body;
+    const { organization_id, quotedPrice, deliveryDate, name, commodityImage, grade, quantity, deliveryLocation, lat, long, quoteExpiry, head_office_id, branch_id, expectedProcurementDate } = req.body;
 
     if (user_type && user_type != _userType.agent)
         return res.send(new serviceResponse({ status: 400, errors: [{ message: _response_message.Unauthorized() }] }))
 
-    const randomVal = _generateOrderNumber();
+    let randomVal
+    let isUnique = false;
 
-    const quote_expiry_date = _addDays(quoteExpiry);
+    while (!isUnique) {
+        randomVal = _generateOrderNumber();
+        const existingReq = await RequestModel.findOne({ reqNo: randomVal });
+        if (!existingReq) {
+            isUnique = true;
+        }
+    }
+
     const delivery_date = moment(deliveryDate).format("YYYY-MM-DD");
 
-    if (moment(delivery_date).isBefore(quote_expiry_date)) {
+    if (moment(delivery_date).isBefore(moment(quoteExpiry))) {
         return res.status(200).send(new serviceResponse({ status: 400, errors: [{ message: _response_message.invalid_delivery_date("Delivery date") }] }))
     }
 
@@ -32,9 +40,9 @@ module.exports.createProcurement = asyncErrorHandler(async (req, res) => {
         reqNo: randomVal,
         expectedProcurementDate,
         quotedPrice, deliveryDate: delivery_date,
-        product: { name, commodityImage, grade, variety, quantity },
+        product: { name, commodityImage, grade, quantity },
         address: { deliveryLocation, lat, long },
-        quoteExpiry: _addDays(quoteExpiry),
+        quoteExpiry: moment(quoteExpiry),
         createdBy: user_id
     });
 
@@ -70,8 +78,7 @@ module.exports.approveRejectOfferByAgent = asyncErrorHandler(async (req, res) =>
 
 module.exports.getProcurement = asyncErrorHandler(async (req, res) => {
 
-    const { page, limit, skip, paginate = 1, sortBy, search = '', status } = req.query
-
+    const { page, limit, skip, paginate = 1, sortBy, search = '' } = req.query
     let query = search ? {
         $or: [
             { "reqNo": { $regex: search, $options: 'i' } },
