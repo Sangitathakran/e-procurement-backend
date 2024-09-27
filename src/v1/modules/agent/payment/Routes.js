64938e4a5a158de1e64ab714d31a2486c92ca710
Sangita <sangita@navankur.org>
2024-09-27 12:36:51 +0530

    payment route

--- src/v1/modules/agent/payment/Routes.js
+++ src/v1/modules/agent/payment/Routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { payment, farmerOrders, associateOrders, paymentApprove, getBill, associateOrdersProceedToPay, batchListProceedToPay, getBillProceedToPay } = require("./Controller");
+const { payment, farmerOrders, associateOrders, paymentApprove, getBill, proceedToPay, associateOrdersProceedToPay, batchListProceedToPay, getBillProceedToPay } = require("./Controller");
 const { verifyAgent } = require("../utils/verifyAgent");
 
 const paymentRoutes = express.Router();
@@ -13,6 +13,6 @@ paymentRoutes.get("/bill-view", verifyAgent, getBill);
 paymentRoutes.get("/proceed-to-pay", verifyAgent, proceedToPay);
 paymentRoutes.get("/proceed-to-pay-associate-orders", verifyAgent, associateOrdersProceedToPay);
 paymentRoutes.get("/proceed-to-pay-batch-list", verifyAgent, batchListProceedToPay);
-paymentRoutes.get("/proceed-to-pay-bill-view", verifyAssociate, getBillProceedToPay);
+paymentRoutes.get("/proceed-to-pay-bill-view", verifyAgent, getBillProceedToPay);
 
 module.exports = { paymentRoutes }; 
