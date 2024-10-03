419b15363831600381a4acef772ba7860d552bdd
Ankush <ankush@navankur.org>
2024-10-03 17:22:18 +0530

    changes

--- src/v1/modules/branch-office/utils/verifyBO.js
+++ src/v1/modules/branch-office/utils/verifyBO.js
@@ -24,7 +24,6 @@ exports.verifyBO = asyncErrorHandler(async (req, res, next) => {
     jwt.verify(token, JWT_SECRET_KEY, function (err, decodedToken) {
         if (err) {
             return res.status(200).send(new serviceResponse({ status: 401, errors: [{ message: _response_message.invalid("token") }] }))
-
         }
         
         if (decodedToken.user_type != _userType.bo) {
