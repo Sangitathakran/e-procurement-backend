0f546d4d6fb817399045ea4db4746221dcbf1015
Ankush <ankush@navankur.org>
2024-10-08 10:18:09 +0530

    work on dashboard api

--- src/v1/modules/agent/dashboard/Controller.js
+++ src/v1/modules/agent/dashboard/Controller.js
@@ -3,11 +3,13 @@ const { serviceResponse } = require("@src/v1/utils/helpers/api_response");
 const { _response_message, _middleware } = require("@src/v1/utils/constants/messages");
 const { ProcurementCenter } = require("@src/v1/models/app/procurement/ProcurementCenter");
 const { FarmerOffers } = require("@src/v1/models/app/procurement/FarmerOffers");
+const { FarmerOrders } = require("@src/v1/models/app/procurement/FarmerOrder");
+const { RequestModel } = require("@src/v1/models/app/procurement/Request");
 const { User } = require("@src/v1/models/app/auth/User");
 const { Branches } = require("@src/v1/models/app/branchManagement/Branches");
 const { farmer } = require("@src/v1/models/app/farmerDetails/Farmer");
 const { decryptJwtToken } = require("@src/v1/utils/helpers/jwt");
-const { _userType, _userStatus, _status, _procuredStatus } = require("@src/v1/utils/constants");
+const { _userType, _userStatus, _status, _procuredStatus, _collectionName, _associateOfferStatus } = require("@src/v1/utils/constants");
 
 
 module.exports.getDashboardStats = async (req, res) => {
@@ -25,22 +27,40 @@ module.exports.getDashboardStats = async (req, res) => {
             is_approved: _userStatus.approved,
             createdAt: { $gte: startOfLastMonth, $lte: endOfLastMonth }
         });
-        console.log('lastMonthAssociates',lastMonthAssociates)
-
+        
         const currentMonthAssociates = await User.countDocuments({
             user_type: _userType.associate,
             is_form_submitted: true,
             is_approved: _userStatus.approved,
             createdAt: { $gte: startOfCurrentMonth }
         });
-        console.log('currentMonthAssociates',currentMonthAssociates)
-
+        
         const difference = currentMonthAssociates - lastMonthAssociates;
         const status = difference >= 0 ? 'increased' : 'decreased';
 
         let differencePercentage = 0;
         if (lastMonthAssociates > 0) {
             differencePercentage = (difference / lastMonthAssociates) * 100;
+        }
+
+        // Farmers stats for last month and current month
+        const lastMonthFarmers = await farmer.countDocuments({
+            status: _status.active,
+            createdAt: { $gte: startOfLastMonth, $lte: endOfLastMonth }
+        });
+
+        const currentMonthFarmers = await farmer.countDocuments({
+            status: _status.active,
+            createdAt: { $gte: startOfCurrentMonth }
+        });
+
+        // Difference and percentage for farmers
+        const farmerDifference = currentMonthFarmers - lastMonthFarmers;
+        const farmerStatus = farmerDifference >= 0 ? 'increased' : 'decreased';
+
+        let farmerDifferencePercentage = 0;
+        if (lastMonthFarmers > 0) {
+            farmerDifferencePercentage = (farmerDifference / lastMonthFarmers) * 100;
         }
 
         const branchOfficeCount = (await Branches.countDocuments({ status: _status.active })) ?? 0;
@@ -57,11 +77,20 @@ module.exports.getDashboardStats = async (req, res) => {
             status: status,
         };
 
+        const farmerStats = {
+            totalFarmers: farmerCount,
+            currentMonthFarmers,
+            lastMonthFarmers,
+            difference: farmerDifference,
+            differencePercentage: farmerDifferencePercentage.toFixed(2) + '%',
+            status: farmerStatus,
+        };
+
         const records = { 
             branchOfficeCount,
             associateStats, 
             procurementCenterCount, 
-            farmerCount 
+            farmerStats 
         };
 
         return res.send(new serviceResponse({ status: 200, data: records, message: _response_message.found("Dashboard Stats") }));
@@ -75,7 +104,32 @@ module.exports.getProcurementsStats = async (req, res) => {
 
     try {
 
-        const procurementsStats = await FarmerOffers.aggregate([
+        const { month, year } = req.query;
+
+        const currentDate = new Date();
+        const currentYear = currentDate.getFullYear();
+
+        if (month && (isNaN(month) || month < 1 || month > 12)) {
+            return res.status(200).send(new serviceResponse({ status: 400, errors: [{ message: _response_message.invalid("month. It should be between 1 and 12.") }] }));
+        }
+
+        if (year && (isNaN(year) || year > currentYear)) {
+            return res.status(200).send(new serviceResponse({ status: 400, errors: [{ message: _response_message.invalid(`year. It should not be greater than ${currentYear}`) }] }));
+        }
+
+        const selectedMonth = month ? parseInt(month) - 1 : currentDate.getMonth();
+        const selectedYear = year ? parseInt(year) : currentDate.getFullYear();
+
+        const startOfMonth = new Date(selectedYear, selectedMonth, 1);
+        const endOfMonth = new Date(selectedYear, selectedMonth + 1, 0, 23, 59, 59, 999);
+        
+
+        const procurementsStats = await FarmerOrders.aggregate([
+            {
+                $match: {
+                    createdAt: { $gte: startOfMonth, $lte: endOfMonth }
+                }
+            },
             {
                 $group: {
                     _id: "$status",
@@ -84,27 +138,142 @@ module.exports.getProcurementsStats = async (req, res) => {
             }
         ]);
 
-        // Initialize counts for each status
         const records = {
-            received: 0,
-            pending: 0,
+            completed: 0,
+            ongoing: 0,
             failed: 0,
+            total: 0,
+            completedPercentage: 0,
+            ongoingPercentage: 0,
+            failedPercentage: 0 
         };
 
-        // Map the counts from the aggregation result
         procurementsStats.forEach(item => {
             if (item._id === _procuredStatus.received) {
-                records.received = item.count;
+                records.completed = item.count;
             } else if (item._id === _procuredStatus.pending) {
-                records.pending = item.count;
+                records.ongoing = item.count;
             } else if (item._id === _procuredStatus.failed) {
                 records.failed = item.count;
             }
+            records.total += item.count;
         });
+
+        if (records.total > 0) {
+            records.completedPercentage = ((records.completed / records.total) * 100).toFixed(2) + '%';
+            records.ongoingPercentage = ((records.ongoing / records.total) * 100).toFixed(2) + '%';
+            records.failedPercentage = ((records.failed / records.total) * 100).toFixed(2) + '%';
+        }
 
         return res.send(new serviceResponse({ status: 200, data: records, message: _response_message.found("Procured Stats") }));
 
     } catch (error) {
         _handleCatchErrors(error, res);
     }
+}
+
+module.exports.getProcurementStatusList = async (req, res) => {
+
+    try {
+        const { page, limit, skip, paginate = 1, sortBy, search = '',} = req.query
+        
+        let query = {
+            ...(search ? { reqNo: { $regex: search, $options: "i" }, deletedAt: null } : { deletedAt: null })
+        };
+        
+        const records = { count: 0 };
+        const selectedFields = 'reqNo quoteExpiry product.name quotedPrice totalQuantity fulfilledQty deliveryDate expectedProcurementDate';
+        const fetchedRecords = paginate == 1
+            ? await RequestModel.find(query)
+                .select(selectedFields)
+                .sort(sortBy)
+                .skip(skip)
+                .limit(parseInt(limit))
+
+            : await RequestModel.find(query).sort(sortBy);
+
+        records.rows = fetchedRecords.map(record => ({
+            orderId: record.reqNo,
+            quoteExpiry: record.quoteExpiry,
+            productName: record.product.name,
+            quotedPrice: record.quotedPrice,
+            deliveryDate: record.deliveryDate,
+            expectedProcurementDate: record.expectedProcurementDate,
+            totalQuantity : record.totalQuantity,
+            fulfilledQty : record.fulfilledQty
+        }));
+            
+        records.count = await RequestModel.countDocuments(query);
+
+        if (paginate == 1) {
+            records.page = page
+            records.limit = limit
+            records.pages = limit != 0 ? Math.ceil(records.count / limit) : 0
+        }
+        
+        return res.send(new serviceResponse({ status: 200, data: records, message: _response_message.found("Procurement") }));
+
+    } catch (error) {
+        _handleCatchErrors(error, res);
+    }
+    
+}
+
+module.exports.getPendingOffersCountByRequestId = async (req, res) => {
+
+    try {
+        const { page, limit, skip, paginate = 1, sortBy, search = '',} = req.query
+        
+        let query = {
+            ...(search && { reqNo: { $regex: search, $options: "i" } })
+        };
+        
+        const aggregationPipeline = [
+            { $match: query },
+            {
+                $lookup: {
+                    from: 'associateoffers',
+                    localField: '_id',
+                    foreignField: 'req_id',
+                    as: 'offers'
+                }
+            },
+            {
+                $addFields: {
+                    pendingOffersCount: { $size: '$offers' }
+                }
+            },
+            {
+                $project: {
+                    reqNo: 1,
+                    seller_id: 1,
+                    quoteExpiry: 1,
+                    'product.name': 1,
+                    quotedPrice: 1,
+                    totalQuantity: 1,
+                    fulfilledQty: 1,
+                    deliveryDate: 1,
+                    expectedProcurementDate: 1,
+                    pendingOffersCount:1,
+                }
+            },
+            { $sort: sortBy ? { [sortBy]: 1 } : { createdAt: -1 } },
+            { $skip: skip },
+            { $limit: parseInt(limit) }
+        ];
+        const records = await RequestModel.aggregate(aggregationPipeline);
+        const count = await RequestModel.countDocuments(query);
+
+        if (paginate == 1) {
+            records.page = page
+            records.limit = limit
+            records.pages = limit != 0 ? Math.ceil(records.count / limit) : 0
+        }
+        
+        return res.send(new serviceResponse({ status: 200, data: records, message: _response_message.found("Procurement") }));
+
+    } catch (error) {
+        _handleCatchErrors(error, res);
+    }
+    
 }
--- src/v1/modules/agent/dashboard/Routes.js
+++ src/v1/modules/agent/dashboard/Routes.js
@@ -1,5 +1,5 @@
 const { _middleware } = require("@src/v1/utils/constants/messages");
-const { getDashboardStats, getProcurementsStats } = require("./Controller");
+const { getDashboardStats, getProcurementsStats, getProcurementStatusList, getPendingOffersCountByRequestId } = require("./Controller");
 const express = require("express");
 const { verifyAgent } = require("../utils/verifyAgent");
 const dashboardRoutes = express.Router();
@@ -7,6 +7,8 @@ const dashboardRoutes = express.Router();
 
 dashboardRoutes.get("/", verifyAgent, getDashboardStats);
 dashboardRoutes.get("/precurement-stats", verifyAgent, getProcurementsStats);
+dashboardRoutes.get("/precurement-list", verifyAgent, getProcurementStatusList);
+dashboardRoutes.get("/pending-precurement-list", verifyAgent, getPendingOffersCountByRequestId);
 
 
 module.exports = { dashboardRoutes }; 
