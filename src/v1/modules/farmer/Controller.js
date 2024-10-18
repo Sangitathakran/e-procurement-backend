61161693f90dc1bcb68b4361373d4d7dd7603bc1
Raj kapoor <raj@navankur.org>
2024-10-18 17:34:23 +0530

    bo farmer listing

--- src/v1/modules/farmer/Controller.js
+++ src/v1/modules/farmer/Controller.js
@@ -547,7 +547,8 @@ module.exports.getBoFarmer = async (req, res) => {
 
     const state_id = await getStateId(state);
     const district_id = await getDistrictId(district);
-    if (!state_id ||district_id) {
+    console.log(district_id);
+    if (!state_id || !district_id) {
       return res.status(400).send({ message: "State ID not found for the user's state." });
     }
 
@@ -567,14 +568,14 @@ module.exports.getBoFarmer = async (req, res) => {
     const totalFarmers = await farmer.countDocuments(query);
 
     if (farmers.length === 0) {
-      return res.status(404).send({ message: `No farmers found in state: ${state}` });
+      return res.status(404).send({ message: `No farmers found in state: ${state} and  District: ${district}`  });
     }
     return res.status(200).send({
       status: 200,
-      data: farmers,
       totalFarmers, 
       currentPage: page,
       totalPages: Math.ceil(totalFarmers / parsedLimit),
+      data: farmers,
       message: `Farmers found in state: ${state}`,
     });
   } catch (error) {
