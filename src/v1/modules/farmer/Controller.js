1d3571f8f2ad18e987f2edef795cb398a943a238
Maneesh Kumar Yadav <maneesh@radiantinfonet.com>
2024-10-21 19:06:22 +0530

    update

--- src/v1/modules/farmer/Controller.js
+++ src/v1/modules/farmer/Controller.js
@@ -211,13 +211,13 @@ module.exports.getFarmerDetails = async (req, res) => {
       if(screenName=='address'){
         const state = await StateDistrictCity.findOne({ "states": { $elemMatch: { "_id": farmerDetails?.address.state_id.toString() } } },{ "states.$": 1 });
   
-        const districts = state.states[0].districts.find(item=>item._id==farmerDetails?.address.district_id.toString())
+        const districts = state?.states[0]?.districts.find(item=>item._id==farmerDetails?.address.district_id.toString())
         
               farmerDetails={
                 ...farmerDetails,
                 address:{
                   ...farmerDetails.address,
-                  state:state.states[0]?.state_title,
+                  state:state?.states[0]?.state_title,
                   district:districts?.district_title
                 }
               }
