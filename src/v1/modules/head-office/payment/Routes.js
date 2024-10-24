d1becf8fa4f5a548570323b9c851f4637315dbbd
LAP-RIPL068\Admin <ayushtiwari653@gmail.com>
2024-10-24 15:34:46 +0530

    ho payment

--- src/v1/modules/head-office/payment/Routes.js
+++ src/v1/modules/head-office/payment/Routes.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const { payment, associateOrders } = require("./Controller");
+const { payment, associateOrders, batchList, batchApprove, qcReport } = require("./Controller");
 const { Auth } = require("@src/v1/middlewares/jwt")
 
 const paymentRoutes = express.Router();
 
 paymentRoutes.get("/", Auth, payment);
 paymentRoutes.get("/associate-orders", Auth, associateOrders);
-// paymentRoutes.get("/batch-list", Auth, batchList);
-// paymentRoutes.put("/batch-approval", Auth, batchApprove);
-// paymentRoutes.get("/lot-list", Auth, lot_list);
-// paymentRoutes.get("/qc-report", Auth, qcReport);
+paymentRoutes.get("/batch-list", Auth, batchList);
+paymentRoutes.put("/batch-approval", Auth, batchApprove);
+paymentRoutes.get("/lot-list", Auth, lot_list);
+paymentRoutes.get("/qc-report", Auth, qcReport);
 // paymentRoutes.patch("/payment-approval", Auth, paymentApprove);
 // paymentRoutes.get("/bill-view", Auth, getBill);
 // paymentRoutes.get("/agent-payment-list", Auth, agentPaymentList);
