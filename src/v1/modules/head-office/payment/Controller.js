5a6d21cc8ba3b22cd224eb34bf71da2b61a6edab
LAP-RIPL068\Admin <ayushtiwari653@gmail.com>
2024-10-29 02:10:34 +0530

    approved-batch-list dummy list

--- src/v1/modules/head-office/payment/Controller.js
+++ src/v1/modules/head-office/payment/Controller.js
@@ -314,6 +314,42 @@ module.exports.qcReport = async (req, res) => {
     }
 }
 
+module.exports.approvedBatchList = async (req, res) => {
+
+    try {
+        const { req_id } = req.query;
+        const { user_type } = req;
+
+        const records = { count: 0 }
+
+        records.rows = [{
+            batchId: "100134",
+            seller_id: { user_code: "AS0123" },
+            delivery_at: "2024-10-28T17:23:02.020+00:00",
+            payment_at: "2024-10-28T17:23:02.020+00:00",
+            qty: 60,
+            amt: "12032390",
+            payment_status: "Pending"
+        }, {
+            batchId: "100134",
+            seller_id: { user_code: "AS0123" },
+            delivery_at: "2024-10-28T17:23:02.020+00:00",
+            payment_at: "2024-10-28T17:23:02.020+00:00",
+            qty: 60,
+            amt: "12032390",
+            payment_status: "Pending"
+        },]
+
+        records.page = 1
+        records.limit = 20
+
+        return res.status(200).send(new serviceResponse({ status: 200, data: records, message: _query.get('Qc Report') }))
+    }
+    catch (error) {
+        _handleCatchErrors(error, res);
+    }
+}
+
 module.exports.lot_list = async (req, res) => {
 
     try {
