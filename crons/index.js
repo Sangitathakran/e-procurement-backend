fdc9267c3f8e68772ff8c77274e5cccaf20a2bca
Maneesh Kumar Yadav <maneesh@radiantinfonet.com>
2024-11-01 17:02:57 +0530

    update index js

--- crons/index.js
+++ crons/index.js
@@ -5,9 +5,11 @@ const fs = require("fs");
 const {
   AgentPaymentFile,
 } = require("@src/v1/models/app/payment/agentPaymentFile");
-const {FarmerPaymentFile}=require("@src/v1/models/app/payment/farmerPaymentFile");
-const {Payment}=require("@src/v1/models/app/procurement/Payment");
-const {farmer}=require("@src/v1/models/app/farmerDetails/Farmer")
+const {
+  FarmerPaymentFile,
+} = require("@src/v1/models/app/payment/farmerPaymentFile");
+const { Payment } = require("@src/v1/models/app/procurement/Payment");
+const { farmer } = require("@src/v1/models/app/farmerDetails/Farmer");
 const xlsx = require("xlsx");
 main().catch((err) => console.log(err));
 
@@ -15,15 +17,13 @@ async function main() {
   cron.schedule("0 9-17/2 * * 1-5", () => {
     sendLog();
   });
-//0 */3 * * *
-//*/30 * * * * *
+  //0 */3 * * *
+  //*/30 * * * * *
   cron.schedule("0 */3 * * *", async () => {
-   
     await downloadAgentFile();
   });
   cron.schedule("*/30 * * * * *", async () => {
     await downloadFarmerFile();
-    
   });
 }
 async function downloadAgentFile() {
@@ -78,15 +78,16 @@ async function downloadAgentFile() {
 async function downloadFarmerFile() {
   console.log("farmer file download running");
   let fileDetails = await FarmerPaymentFile.find({ file_status: "upload" });
-     
+
   // console.log(fileDetails)
   //let fileDetails=[{fileName:"AIZER29102024002.xlsx"}]
   for (let item of fileDetails) {
-        item.fileName=item.fileName;
-       await item.save();
-      let paymentDetails= await Payment.findById(item.payment_id).populate('farmer_id');
-      
-      console.log(paymentDetails)
+    item.fileName = item.fileName;
+    await item.save();
+    let paymentDetails = await Payment.findById(item.payment_id).populate(
+      "farmer_id"
+    );
+
     const url = `https://testbank.navbazar.com/v1/download-file?fileName=R_${item.fileName}`; // Replace with your URL
 
     const response = await axios.get(url, {
@@ -110,12 +111,15 @@ async function downloadFarmerFile() {
       // console.log("item", item);
       for (let item2 of sheetData) {
         rowsDetails.push({ ...item2 });
-         if(item2.LIQ_STATUS=='Paid' && paymentDetails.farmer_id.bank_details.account_no==item2.BENEF_ACCOUNT_NMBR){
-          paymentDetails.payment_status='Completed';
-          paymentDetails.transaction_id=item2.UTR_SR_NO;
-           await paymentDetails.save(); 
-
-         }
+        if (
+          item2.LIQ_STATUS == "Paid" &&
+          paymentDetails.farmer_id.bank_details.account_no ==
+            item2.BENEF_ACCOUNT_NMBR
+        ) {
+          paymentDetails.payment_status = "Completed";
+          paymentDetails.transaction_id = item2.UTR_SR_NO;
+          await paymentDetails.save();
+        }
         // CORPORATION_CODE: '101923545',
         // CLIENT_CODE: 'NCCFMAIZER',
         // ACCOUNT_NMBR: '2244102000000055',
