b9342f74bae2a8243a6a8499924db3017b015757
Sangita <sangita@navankur.org>
2024-11-04 12:02:01 +0530

    track order pagination error resolved in Agent

--- src/v1/modules/agent/track_order/Controller.js
+++ src/v1/modules/agent/track_order/Controller.js
@@ -34,15 +34,15 @@ module.exports.getProcurement = asyncErrorHandler(
             },
             { $unwind: '$myoffer' },
             { $match: { 'myoffer.status': { $in: [_associateOfferStatus.ordered, _associateOfferStatus.partially_ordered] } } },
-            { $limit: limit ? parseInt(limit) : 10 },
             ...(sortBy ? [{ $sort: { [sortBy]: 1 } }] : []),  // Sorting if required
-            ...(paginate == 1 ? [{ $skip: parseInt(skip) }, { $limit: parseInt(limit) }] : []) // Pagination if required
+            ...(paginate == 1 ? [{ $skip: parseInt(skip) }, { $limit: parseInt(limit) }] : []), // Pagination if required
+            { $limit: limit ? parseInt(limit) : 10 }
         ];
 
         const records = {};
-        records.rows = await RequestModel.aggregate(pipeline);
         records.count = await RequestModel.countDocuments(query);
-
+        records.rows = await RequestModel.aggregate(pipeline);
+       
 
         if (paginate == 1) {
             records.page = page;
