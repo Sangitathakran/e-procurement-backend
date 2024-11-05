dcf8942cf78410f21967dd206fc16ff3820059c9
LAP-RIPL068\Admin <ayushtiwari653@gmail.com>
2024-11-05 10:24:45 +0530

    login bug

--- src/v1/models/app/procurement/FarmerOrder.js
+++ src/v1/models/app/procurement/FarmerOrder.js
@@ -14,6 +14,7 @@ const farmerOrderSchema = new mongoose.Schema({
     order_no: { type: String, required: false, trim: true },
     receving_date: { type: Date },
     qtyProcured: { type: Number },
+    qtyRemaining: { type: Number, default: 0 },
     procurementCenter_id: { type: mongoose.Schema.Types.ObjectId, ref: _collectionName.ProcurementCenter },
     weighbridge_name: { type: String, trim: true },
     weighbridge_no: { type: Number },
@@ -24,7 +25,6 @@ const farmerOrderSchema = new mongoose.Schema({
     payment_date: { type: Date },
     payment_status: { type: String, enum: ["pending", "credited"], defualt: "pending" },
     net_pay: { type: Number, default: 0 },
-    deliveredQty: { type: Number, default: 0 },
     status: { type: String, enum: Object.values(_procuredStatus), default: _procuredStatus.pending },
     ..._commonKeys
 }, { timestamps: true });
