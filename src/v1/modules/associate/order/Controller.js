6aad5f543e02046b5c1acf397087c7a456d11944
Sangita <sangita@navankur.org>
2024-11-27 18:39:29 +0530

    internal server error in mail functionality

--- src/v1/modules/associate/order/Controller.js
+++ src/v1/modules/associate/order/Controller.js
@@ -143,16 +143,14 @@ module.exports.batch = async (req, res) => {
             'basic_details.associate_details.email': { $exists: true }
         }).select('basic_details.associate_details.email basic_details.associate_details.associate_name');
 
-        // try {
-            await Promise.all(
-                users.map(({ basic_details: { associate_details } }) => {
-                    const { email, associate_name } = associate_details;
-                    return emailService.sendCreateBatchEmail(email, associate_name);
-                })
-            );
-        // } catch (error) {
-        //     _handleCatchErrors(error, res);
-        // }
+
+        await Promise.all(
+            users.map(({ basic_details: { associate_details } }) => {
+                const { email, associate_name } = associate_details;
+                
+                return emailService.sendCreateBatchEmail(email, associate_name);
+            })
+        );
 
         return res.status(200).send(new serviceResponse({ status: 200, data: batchCreated, message: _response_message.created("batch") }))
 
@@ -167,7 +165,7 @@ module.exports.editTrackDelivery = async (req, res) => {
 
     try {
 
-        const { form_type, id, material_img = [], weight_slip = [], procurementExp, qc_survey, gunny_bags, weighing_stiching, loading_unloading, transportation, driage, storageExp,  qc_report = [], lab_report = [], name, contact, license, aadhar, licenseImg, service_name, vehicleNo, vehicle_weight, loaded_weight, gst_number, pan_number, intransit_weight_slip, no_of_bags, weight } = req.body;
+        const { form_type, id, material_img = [], weight_slip = [], procurementExp, qc_survey, gunny_bags, weighing_stiching, loading_unloading, transportation, driage, storageExp, qc_report = [], lab_report = [], name, contact, license, aadhar, licenseImg, service_name, vehicleNo, vehicle_weight, loaded_weight, gst_number, pan_number, intransit_weight_slip, no_of_bags, weight } = req.body;
         const { user_id } = req
 
         const record = await Batch.findOne({ _id: id });
@@ -182,7 +180,7 @@ module.exports.editTrackDelivery = async (req, res) => {
                     return res.status(400).send(new serviceResponse({ status: 400, errors: [{ message: "Your batch is already mark ready " }] }));
                 }
 
-                if (material_img && weight_slip && procurementExp && qc_survey && gunny_bags && weighing_stiching && loading_unloading && transportation && driage && storageExp  && qc_report && lab_report) {
+                if (material_img && weight_slip && procurementExp && qc_survey && gunny_bags && weighing_stiching && loading_unloading && transportation && driage && storageExp && qc_report && lab_report) {
                     // const RateOfProcurement = 840.00;
                     // const RateOfDriage = 100.00;
                     const RateOfStorage = 160.00;
