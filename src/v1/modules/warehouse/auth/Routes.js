fc6969e8cdd14d06e14ff2341ab767c5190d081e
nagma <nagma@navankur.org>
2024-12-09 15:53:58 +0530

    verifyWarehouse added

--- src/v1/modules/warehouse/auth/Routes.js
+++ src/v1/modules/warehouse/auth/Routes.js
@@ -1,17 +1,16 @@
 const { sendOtp, loginOrRegister, saveWarehouseDetails, onboardingStatus, formPreview, findUserStatus, finalFormSubmit, editOnboarding, associateBulkuplod } = require("./Controller")
 // const { validateForm } = require("@src/v1/modules/associate/auth/Validation")
 const express = require("express");
-// const { verifyAssociate } = require("../utils/verifyAssociate");
+const { verifyWarehouse } = require("../utils/verifyWarehouse");
 const wareHouseAuthRoutes = express.Router();
-const { Auth } = require("@src/v1/middlewares/jwt")
 
 wareHouseAuthRoutes.post("/send-otp", sendOtp);
 wareHouseAuthRoutes.post("/register-login", loginOrRegister);
-wareHouseAuthRoutes.put("/onboarding", saveWarehouseDetails);
-wareHouseAuthRoutes.get("/onboarding", Auth, formPreview);
-wareHouseAuthRoutes.get("/onboarding-status", Auth, onboardingStatus);
-wareHouseAuthRoutes.get("/find-user-status", findUserStatus);
-wareHouseAuthRoutes.patch("/final-submit", finalFormSubmit);
+wareHouseAuthRoutes.put("/onboarding", verifyWarehouse, saveWarehouseDetails);
+wareHouseAuthRoutes.get("/onboarding", verifyWarehouse, formPreview);
+wareHouseAuthRoutes.get("/onboarding-status", verifyWarehouse, onboardingStatus);
+wareHouseAuthRoutes.get("/find-user-status", verifyWarehouse, findUserStatus);
+wareHouseAuthRoutes.patch("/final-submit", verifyWarehouse, finalFormSubmit);
 
 // userAuthRoutes.get("/editOnboarding", verifyAssociate, editOnboarding);
 module.exports = { wareHouseAuthRoutes }; 
