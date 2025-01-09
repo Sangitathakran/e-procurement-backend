41bcd2c4654ff41e7da3218c9905d65cfdb9eaf2
Sangita <sangita@navankur.org>
2025-01-09 12:12:56 +0530

    MOU list using aggregation

--- src/v1/modules/nccf/onboarding-request/Controller.js
+++ src/v1/modules/nccf/onboarding-request/Controller.js
@@ -11,8 +11,7 @@ const { asyncErrorHandler } = require("@src/v1/utils/helpers/asyncErrorHandler")
 
 module.exports.getPendingDistillers = asyncErrorHandler(async (req, res) => {
     const { page = 1, limit = 10, skip = 0, paginate = 1, sortBy = "_id", search = '', isExport = 0 } = req.query;
-    const { user_id } = req;
-
+  
     let matchStage = {
         is_approved: _userStatus.pending,
         deletedAt: null,
@@ -65,12 +64,12 @@ module.exports.getPendingDistillers = asyncErrorHandler(async (req, res) => {
 
 module.exports.getDistillerById = asyncErrorHandler(async (req, res) => {
     const { id } = req.params;
-    
+
     let matchStage = {
         _id: new mongoose.Types.ObjectId(id),
         deletedAt: null,
     };
-    
+
     let aggregationPipeline = [
         { $match: matchStage },
         {
@@ -89,40 +88,41 @@ module.exports.getDistillerById = asyncErrorHandler(async (req, res) => {
                 as: "storageFacility"
             }
         },
-        { $project: {
-            _id: 1,
-            'distiller_id': '$user_code',
-            'distiller_details': '$basic_details.distiller_details',
-            'company_owner_info': '$basic_details.company_owner_info',
-            'poc': '$basic_details.point_of_contact',
-            'registered_address': '$address.registered',
-            'operational_address': '$address.operational',
-            'request_date': '$createdAt',
-            'status': '$is_approved',
-            company_details: 1,
-            authorised: 1,
-            bank_details: 1,
-            manufacturingUnit: 1,
-            storageFacility: 1
-        }}
+        {
+            $project: {
+                _id: 1,
+                'distiller_id': '$user_code',
+                'distiller_details': '$basic_details.distiller_details',
+                'company_owner_info': '$basic_details.company_owner_info',
+                'poc': '$basic_details.point_of_contact',
+                'registered_address': '$address.registered',
+                'operational_address': '$address.operational',
+                'request_date': '$createdAt',
+                'status': '$is_approved',
+                company_details: 1,
+                authorised: 1,
+                bank_details: 1,
+                manufacturingUnit: 1,
+                storageFacility: 1
+            }
+        }
     ];
-    
+
     const record = await Distiller.aggregate(aggregationPipeline);
-    
+
     if (!record || record.length === 0) {
-        return res.status(404).send(new serviceResponse({ 
-            status: 404, 
-            message: _response_message.notFound("Pending Distiller") 
+        return res.status(404).send(new serviceResponse({
+            status: 404,
+            message: _response_message.notFound("Pending Distiller")
         }));
     }
-    
-    return res.status(200).send(new serviceResponse({ 
-        status: 200, 
-        data: record[0], 
-        message: _response_message.found("Pending Distiller") 
+
+    return res.status(200).send(new serviceResponse({
+        status: 200,
+        data: record[0],
+        message: _response_message.found("Pending Distiller")
     }));
 });
-
 
 module.exports.updateApprovalStatus = asyncErrorHandler(async (req, res) => {
     const { id } = req.query;
@@ -148,24 +148,58 @@ module.exports.updateApprovalStatus = asyncErrorHandler(async (req, res) => {
 });
 
 module.exports.getPendingMouList = asyncErrorHandler(async (req, res) => {
-    const { page, limit, skip, paginate = 1, sortBy, search = '', isExport = 0 } = req.query
-    const { user_id } = req;
-    let query = {
+
+    const { page = 1, limit = 10, skip = 0, paginate = 1, sortBy = "_id", search = '', isExport = 0 } = req.query;
+  
+    let matchStage = {
         is_approved: _userStatus.approved,
-        ...(search ? { orderId: { $regex: search, $options: "i" }, deletedAt: null } : { deletedAt: null })
+        deletedAt: null,
     };
-    const records = { count: 0 };
-    records.rows = paginate == 1 ? await Distiller.find(query)
-        .sort(sortBy)
-        .skip(skip)
-        .limit(parseInt(limit)) : await Distiller.find(query).sort(sortBy);
-    records.count = await Distiller.countDocuments(query);
-    if (paginate == 1) {
-        records.page = page
-        records.limit = limit
-        records.pages = limit != 0 ? Math.ceil(records.count / limit) : 0
+
+    if (search) {
+        matchStage.orderId = { $regex: search, $options: "i" };
     }
-    return res.status(200).send(new serviceResponse({ status: 200, data: records, message: _response_message.found("Pending Distiller") }))
+
+    let aggregationPipeline = [
+        { $match: matchStage },
+        { $sort: { [sortBy]: 1 } },
+        {
+            $project: {
+                _id: 1,
+                'distiller_id': '$user_code',
+                'distiller_name': '$basic_details.distiller_details.organization_name',
+                'poc': '$basic_details.point_of_contact.name',
+                'address': '$address.registered',
+                'request_date': '$createdAt',
+                'status': '$mou_approval',
+                'hard_copy': '$mou',
+            }
+        }
+    ];
+
+    if (paginate == 1) {
+        aggregationPipeline.push(
+            { $skip: parseInt(skip) },
+            { $limit: parseInt(limit) }
+
+        );
+    }
+
+    const records = { count: 0 };
+    records.rows = await Distiller.aggregate(aggregationPipeline);
+    records.count = await Distiller.countDocuments(matchStage);
+
+    if (paginate == 1) {
+        records.page = page;
+        records.limit = limit;
+        records.pages = limit != 0 ? Math.ceil(records.count / limit) : 0;
+    }
+
+    return res.status(200).send(new serviceResponse({
+        status: 200,
+        data: records,
+        message: _response_message.found("Distiller MOU")
+    }));
 });
 
 module.exports.updateMouApprovalStatus = asyncErrorHandler(async (req, res) => {
--- src/v1/modules/nccf/onboarding-request/Routes.js
+++ src/v1/modules/nccf/onboarding-request/Routes.js
@@ -12,9 +12,9 @@ const nccfOnboardingRoutes = express.Router();
 // nccfOnboardingRoutes.patch("/mou-approval", Auth, updateMouApprovalStatus);
 
 nccfOnboardingRoutes.get("/", getPendingDistillers);
+nccfOnboardingRoutes.get("/pending-Mou-list", getPendingMouList);
 nccfOnboardingRoutes.get('/:id', getDistillerById);
 nccfOnboardingRoutes.patch("/distillers-approve", updateApprovalStatus);
-nccfOnboardingRoutes.get("/pending-Mou-list", getPendingMouList);
 nccfOnboardingRoutes.patch("/mou-approval", updateMouApprovalStatus);
 
 module.exports = { nccfOnboardingRoutes }; 
