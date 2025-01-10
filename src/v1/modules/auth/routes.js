9f15adce528356137ef34ce507f9b28ff58160d1
TejasBishnoi <tejasvi@radiantinfonet.com>
2025-01-10 12:48:30 +0530

    auth/aadharValidationUIDAI-APIs_developed

--- src/v1/modules/auth/routes.js
+++ src/v1/modules/auth/routes.js
@@ -3,9 +3,11 @@ const authRoutes = express.Router()
 
 const { emailAuthRoutes } = require("./email-auth/Routes")
 const { mobileAuthRoutes } = require("./mobile-auth/Routes")
+const { aadharAuthRoutes } = require("./aadhar-auth/Routes");
 
 authRoutes.use("/email", emailAuthRoutes)
 authRoutes.use("/mobile", mobileAuthRoutes)
+authRoutes.use("/aadhar", aadharAuthRoutes)
 
 
 module.exports = { authRoutes } 
