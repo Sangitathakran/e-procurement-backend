957f25a22f650d595a061e7745a2458703f087d8
Sangita <sangita@navankur.org>
2025-01-10 16:37:56 +0530

    user-management issue-resolved

--- src/v1/modules/agent/auth/Controllers.js
+++ src/v1/modules/agent/auth/Controllers.js
@@ -48,25 +48,25 @@ module.exports.createAgency = async (req, res) => {
 
         const existUser = await Agency.findOne({ email: email });
 
-        if (existUser){
+        if (existUser) {
             return res.send(new serviceResponse({ status: 400, errors: [{ message: _response_message.allReadyExist('Email') }] }))
         }
 
         // checking the existing user in Master User collection
         const isUserAlreadyExist = await MasterUser.findOne(
-              { $or: [{ mobile: { $exists: true, $eq: phone.trim() } }, { email: { $exists: true, $eq: email.trim() } }] });
-          
-        if(isUserAlreadyExist){
-          return sendResponse({res, status: 400, message: "user already existed with this mobile number or email in Master"})
-        }
-        
-        const password = generateRandomPassword();
+            { $or: [{ mobile: { $exists: true, $eq: phone.trim() } }, { email: { $exists: true, $eq: email.trim() } }] });
 
+        if (isUserAlreadyExist) {
+            return sendResponse({ res, status: 400, message: "user already existed with this mobile number or email in Master" })
+        }
+
+        const password = generateRandomPassword();
+        console.log(password);
         const hashedPassword = await bcrypt.hash(password, 10);
         const agency = new Agency({
-            agent_name : agent_name,
-            email : email,
-            phone : phone,
+            agent_name: agent_name,
+            email: email,
+            phone: phone,
         });
 
         const savedAgency = await agency.save();
@@ -74,34 +74,34 @@ module.exports.createAgency = async (req, res) => {
         const login_url = `${process.env.FRONTEND_URL}${_frontendLoginRoutes.agent}`
         const emailPayload = {
             email: savedAgency.email,
-            user_name:savedAgency.first_name,
+            user_name: savedAgency.first_name,
             name: savedAgency.first_name,
             password: password,
-            login_url:login_url
+            login_url: login_url
         }
         await emailService.sendAgencyCredentialsEmail(emailPayload);
 
-        const type = await TypesModel.findOne({_id:"67110114f1cae6b6aadc2425"})
+        const type = await TypesModel.findOne({ _id: "67110114f1cae6b6aadc2425" })
 
-        if(savedAgency._id){
+        if (savedAgency._id) {
             const masterUser = new MasterUser({
-                firstName : agent_name,
-                isAdmin : true,
-                email : email.trim(),
-                mobile : phone.trim(),
+                firstName: agent_name,
+                isAdmin: true,
+                email: email.trim(),
+                mobile: phone.trim(),
                 password: hashedPassword,
-                user_type : type.user_type,
+                user_type: type.user_type,
                 createdBy: req.user._id,
                 userRole: [type.adminUserRoleId],
                 portalId: savedAgency._id,
-                ipAddress:getIpAddress(req) 
+                ipAddress: getIpAddress(req)
             });
-    
+
             await masterUser.save();
-        }else{
-            await Agency.deleteOne({_id:savedAgency._id})
+        } else {
+            await Agency.deleteOne({ _id: savedAgency._id })
             throw new Error('Agency not created ')
-            
+
         }
 
         return res.status(200).send(new serviceResponse({ message: _response_message.created('Agency'), data: savedAgency }));
@@ -112,23 +112,23 @@ module.exports.createAgency = async (req, res) => {
 
 module.exports.changeStatus = async (req, res) => {
     try {
-      const agentId  = req.params.id; 
-      if(!agentId){
-        return sendResponse({res, status: 400, message: "agent id not provided"})
-      }
-      const agent = await Agency.findById(agentId);
-      if(!agent){
-        return sendResponse({res, status: 400, message: "agent not exist or wrong agent id"})
-      }
-  
-      agent.status = agent?.status === 'active' ? 'inactive' : 'active';
-  
-      const updatedagent = await agent.save();
-      return sendResponse({res, status: 200, data: updatedagent, message: "user status changed successfully"})
-  
+        const agentId = req.params.id;
+        if (!agentId) {
+            return sendResponse({ res, status: 400, message: "agent id not provided" })
+        }
+        const agent = await Agency.findById(agentId);
+        if (!agent) {
+            return sendResponse({ res, status: 400, message: "agent not exist or wrong agent id" })
+        }
+
+        agent.status = agent?.status === 'active' ? 'inactive' : 'active';
+
+        const updatedagent = await agent.save();
+        return sendResponse({ res, status: 200, data: updatedagent, message: "user status changed successfully" })
+
     } catch (err) {
-      _handleCatchErrors(error, res);
+        _handleCatchErrors(error, res);
     }
-  };
+};
 
 
