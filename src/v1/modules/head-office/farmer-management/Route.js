92050f0448d42696bda70929e4025359922b22fd
Sourav Roy <sourav@radiantinfonet.com>
2025-01-29 18:41:12 +0530

    created api for farmer

--- src/v1/modules/head-office/farmer-management/Route.js
+++ src/v1/modules/head-office/farmer-management/Route.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const farmerManagementRoutes = express.Router();
 
-const {farmerList, getSingleFarmer } = require("./Controller")
+const {farmerList, getSingleFarmer, getAllStateAndDistricts,getStatewiseFarmersCount } = require("./Controller")
 
-farmerManagementRoutes.get("/farmer-list",farmerList);
-farmerManagementRoutes.get("/single-farmer/:id",getSingleFarmer);
-
-
-
+farmerManagementRoutes.get("/farmer-list", farmerList);
+farmerManagementRoutes.get("/farmer-address-list", getAllStateAndDistricts);
+farmerManagementRoutes.get("/farmers-count-list-statewise", getStatewiseFarmersCount);
+farmerManagementRoutes.get("/single-farmer/:id", getSingleFarmer);
 
 module.exports = { farmerManagementRoutes }; 
