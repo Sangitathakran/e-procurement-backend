346e88521cc16acd7b42e094cb6c0bb7b708e5b0
wajahat <wajahat@radiantinfonet.com>
2025-02-03 13:14:27 +0530

    farmer-count-list-stateWise issue fixed

--- src/v1/modules/head-office/farmer-management/Route.js
+++ src/v1/modules/head-office/farmer-management/Route.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const farmerManagementRoutes = express.Router();
 
-const {farmerList, getSingleFarmer, getAllStateAndDistricts } = require("./Controller")
+const {farmerList, getSingleFarmer, getAllStateAndDistricts,getStatewiseFarmersCount } = require("./Controller")
 
 farmerManagementRoutes.get("/farmer-list",farmerList);
 farmerManagementRoutes.get("/single-farmer/:id",getSingleFarmer);
 farmerManagementRoutes.get("/farmer-address-list", getAllStateAndDistricts);
+farmerManagementRoutes.get("/farmers-count-list-statewise", getStatewiseFarmersCount);
 
 
 
