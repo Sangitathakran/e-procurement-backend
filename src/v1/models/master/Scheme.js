189ca9f7f5ea369d19614ec9ebfa486b3182c4a9
Sangita <sangita@navankur.org>
2025-02-07 10:29:09 +0530

    scheme and commodity, requirement changes

--- src/v1/models/master/Scheme.js
+++ src/v1/models/master/Scheme.js
@@ -1,4 +1,5 @@
-const { _collectionName } = require("@src/v1/utils/constants");
+const { _collectionName, _status, _season, _period, _centralNodalAgency } = require("@src/v1/utils/constants");
+
 const mongoose = require("mongoose");
 const SchemeSchema = new mongoose.Schema(
   {
@@ -7,11 +8,15 @@ const SchemeSchema = new mongoose.Schema(
     //   ref: _collectionName.Agency,
     //   required: true,
     // },
-    schmeCode: { type: String, unique: true }, 
+    schmeCode: { type: String, unique: true },
     schemeName: { type: String, required: true },
-    season: { type: String, enum: ["Kharif", "Rabi", "Zaid"] },
-    period: { type: String, enum: ["2024", "2024-2025", "2025"] },
-    centralNodalAgency: { type: String, enum: ["nodal1", "nodal2", "nodal3"] },
+    // season: { type: String, enum: ["Kharif", "Rabi", "Zaid"] },
+    // period: { type: String, enum: ["2024", "2024-2025", "2025"] },
+    // centralNodalAgency: { type: String, enum: ["nodal1", "nodal2", "nodal3"] },
+
+    season: { type: String, enum: Object.values(_season), default: _season.Kharif },
+    period: { type: String, enum: Object.values(_period), default: _period.currentYear },
+    centralNodalAgency: { type: String, enum: Object.values(_centralNodalAgency), default: _centralNodalAgency.nodal1 },
     procurement: { type: Number, required: true },
     commodity: { type: String, required: true },
     status: { type: String, enum: Object.values(_status), default: _status.active },
