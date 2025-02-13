0b47ef9be590abd906784180e6f867c9a1ffc088
Sangita <sangita@navankur.org>
2025-02-13 19:03:40 +0530

    commodity changes in Scheme

--- src/v1/models/master/Scheme.js
+++ src/v1/models/master/Scheme.js
@@ -10,7 +10,11 @@ const SchemeSchema = new mongoose.Schema(
     period: { type: String, enum: Object.values(_period), default: _period.currentYear },
     centralNodalAgency: { type: String, enum: Object.values(_centralNodalAgency), default: _centralNodalAgency.nodal1 },
     procurement: { type: Number, required: true },
-    commodity: { type: String, required: true },
+    commodity_id:{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: _collectionName.Commodity,
+      required: true,
+    },
     procurementDuration: { type: String, required: true },
     schemeApprovalLetter: { type: String },
     status: { type: String, enum: Object.values(_status), default: _status.active },
