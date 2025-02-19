10355896a0c7938716e8b28f71acb27eec31499a
Sangita <sangita@navankur.org>
2025-02-19 15:13:34 +0530

    assign SLA, Get Assigned SLA API

--- src/v1/modules/agent/sla-management/Controller.js
+++ src/v1/modules/agent/sla-management/Controller.js
@@ -3,6 +3,9 @@ const { _response_message } = require("@src/v1/utils/constants/messages");
 const { _handleCatchErrors } = require("@src/v1/utils/helpers");
 const { serviceResponse } = require("@src/v1/utils/helpers/api_response");
 const { asyncErrorHandler } = require("@src/v1/utils/helpers/asyncErrorHandler");
+const { Scheme } = require("@src/v1/models/master/Scheme");
+const { SchemeAssign } = require("@src/v1/models/master/SchemeAssign");
+const { mongoose } = require("mongoose");
 
 module.exports.createSLA = asyncErrorHandler(async (req, res) => {
     try {
@@ -404,3 +407,114 @@ module.exports.addSchemeToSLA = asyncErrorHandler(async (req, res) => {
     }
 });
 
+module.exports.schemeAssign = asyncErrorHandler(async (req, res) => {
+    try {
+        const { scheme_id, bo_id, sla_id } = req.body;
+
+        const records = await SchemeAssign.create({ bo_id, scheme_id, sla_id })
+
+        return res.status(200).send(
+            new serviceResponse({
+                status: 200,
+                data: records,
+                message: _response_message.created("Scheme Assign"),
+            })
+        );
+    } catch (error) {
+        _handleCatchErrors(error, res);
+    }
+});
+
+module.exports.getAssignedScheme = async (req, res) => {
+    const { sla_id, page = 1, limit = 10, skip = 0, paginate = 1, sortBy, search = '', isExport = 0 } = req.query;
+
+    // Initialize matchQuery
+    let matchQuery = { sla_id: new mongoose.Types.ObjectId(sla_id) };
+
+    // Validate ObjectId
+    if (!mongoose.Types.ObjectId.isValid(sla_id)) {
+        return res.status(400).json({ message: "Invalid SLA ID" });
+    }
+
+    let aggregationPipeline = [
+        { $match: matchQuery },
+        {
+            $lookup: {
+                from: 'branches',
+                localField: 'bo_id',
+                foreignField: '_id',
+                as: 'branchDetails',
+            },
+        },
+        { $unwind: { path: '$branchDetails', preserveNullAndEmptyArrays: true } },
+        {
+            $lookup: {
+                from: "schemes", // Adjust this to your actual collection name for branches
+                localField: "scheme_id",
+                foreignField: "_id",
+                as: "schemeDetails"
+            }
+        },
+        { $unwind: { path: "$schemeDetails", preserveNullAndEmptyArrays: true } },
+        {
+            $project: {
+                _id: 1,
+                schemeId: '$schemeDetails.schemeId',
+                // schemeName: '$schemeDetails.schemeName',
+                schemeName: {
+                    $concat: [
+                        "$schemeDetails.schemeName", "",
+                        { $ifNull: ["$schemeDetails.commodityDetails.name", ""] }, "",
+                        { $ifNull: ["$schemeDetails.season", ""] }, "",
+                        { $ifNull: ["$schemeDetails.period", ""] }
+                    ]
+                },
+                branchName: '$branchDetails.branchName',
+                createdOn: '$createdAt'
+            }
+        }
+    ];
+    if (paginate == 1) {
+        aggregationPipeline.push(
+            { $sort: { [sortBy || 'createdAt']: -1, _id: -1 } }, // Secondary sort by _id for stability
+            { $skip: parseInt(skip) },
+            { $limit: parseInt(limit) }
+        );
+    } else {
+        aggregationPipeline.push({ $sort: { [sortBy || 'createdAt']: -1, _id: -1 } },);
+    }
+
+    const rows = await SchemeAssign.aggregate(aggregationPipeline);
+    const countPipeline = [
+        { $match: matchQuery },
+        { $count: "total" }
+    ];
+    const countResult = await SchemeAssign.aggregate(countPipeline);
+    const count = countResult[0]?.total || 0;
+    const records = { rows, count };
+    if (paginate == 1) {
+        records.page = parseInt(page);
+        records.limit = parseInt(limit);
+        records.pages = limit != 0 ? Math.ceil(count / limit) : 0;
+    }
+    if (isExport == 1) {
+        const record = rows.map((item) => {
+            return {
+                "Scheme Id": item?.schemeId || "NA",
+                "schemeName": item?.schemeName || "NA",
+                "branchName": item?.branchName || "NA",
+            };
+        });
+        if (record.length > 0) {
+            dumpJSONToExcel(req, res, {
+                data: record,
+                fileName: `Scheme-record.xlsx`,
+                worksheetName: `Scheme-record`
+            });
+        } else {
+            return res.status(200).send(new serviceResponse({ status: 200, data: records, message: _response_message.notFound("Scheme Assign") }));
+        }
+    } else {
+        return res.status(200).send(new serviceResponse({ status: 200, data: records, message: _response_message.found("Scheme Assign") }));
+    }
+}
--- src/v1/modules/agent/sla-management/Routes.js
+++ src/v1/modules/agent/sla-management/Routes.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const { createSLA, getSLAList, deleteSLA, updateSLA, getSLAById, updateSLAStatus, addSchemeToSLA } = require("./Controller");
+const { createSLA, getSLAList, deleteSLA, updateSLA, getSLAById, updateSLAStatus, addSchemeToSLA, schemeAssign, getAssignedScheme } = require("./Controller");
 const { Auth } = require("@src/v1/middlewares/jwt");
 
 const slaRoute = express.Router();
+
+slaRoute.post("/schemeAssign", Auth, schemeAssign);
+slaRoute.get("/getAssignedScheme", Auth, getAssignedScheme);
 
 slaRoute.post("/", createSLA);
 slaRoute.get("/", getSLAList);
@@ -11,5 +14,6 @@ slaRoute.put("/:sla_id", updateSLA);
 slaRoute.patch("/:sla_id", updateSLAStatus);
 slaRoute.post("/:sla_id/add-scheme", addSchemeToSLA);
 slaRoute.delete("/:sla_id", deleteSLA);
+
 
 module.exports = { slaRoute };
