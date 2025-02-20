0949319818415fe1747e046d51fc00d7744e0da6
Raman Madhukar <raman@navankur.org>
2025-02-20 10:12:08 +0000

    adding Auth in sla

--- src/v1/modules/agent/sla-management/Routes.js
+++ src/v1/modules/agent/sla-management/Routes.js
@@ -5,7 +5,7 @@ const { Auth } = require("@src/v1/middlewares/jwt");
 const slaRoute = express.Router();
 
 slaRoute.post("/schemeAssign", Auth, schemeAssign);
-slaRoute.get("/getAssignedScheme", getAssignedScheme);
+slaRoute.get("/getAssignedScheme", Auth, getAssignedScheme);
 
 slaRoute.post("/", createSLA);
 slaRoute.get("/", getSLAList);
