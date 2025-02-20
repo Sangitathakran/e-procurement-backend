01fc706d5a2ab026bdafc879eb6e69278d64962f
Sangita <sangita@navankur.org>
2025-02-20 16:26:14 +0530

    added new fields in requrest list in Agent portal

--- src/v1/modules/agent/scheme/Controller.js
+++ src/v1/modules/agent/scheme/Controller.js
@@ -180,14 +180,6 @@ module.exports.getSchemeById = asyncErrorHandler(async (req, res) => {
         _id: 1,
         schemeId: 1,
         schemeName: 1,
-        // schemeName: {
-        //   $concat: [
-        //     "$schemeName", "",
-        //     { $ifNull: ["$commodityDetails.name", ""] }, "",
-        //     { $ifNull: ["$season", ""] }, "",
-        //     { $ifNull: ["$period", ""] }
-        //   ]
-        // },
         commodity_id: 1,
         season: 1,
         period: 1,
