596abbb732c830ea7bac5c089c97e13d3f16a20d
Sangita <sangita@navankur.org>
2025-02-24 10:46:54 +0530

    assigned schme bo list and sla list

--- src/v1/modules/head-office/scheme/Controller.js
+++ src/v1/modules/head-office/scheme/Controller.js
@@ -232,7 +232,10 @@ module.exports.getAssignedScheme = asyncErrorHandler(async (req, res) => {
     return res.status(400).json({ message: "Invalid scheme ID" });
   }
 
-  let matchQuery = { scheme_id: new mongoose.Types.ObjectId(scheme_id) };
+  let matchQuery = {
+    scheme_id: new mongoose.Types.ObjectId(scheme_id),
+    bo_id: { $exists: true, $ne: null }
+  };
 
   let aggregationPipeline = [
     { $match: matchQuery },
@@ -257,6 +260,11 @@ module.exports.getAssignedScheme = asyncErrorHandler(async (req, res) => {
     {
       $project: {
         _id: 1,
+        branchName: "$branchDetails.branchName",
+        branchLocation: "$branchDetails.state",
+        targetAchieved: '$schemeDetails.procurement',
+        bo_id: 1,
+        assignQty: 1,
         schemeId: "$schemeDetails.schemeId",
         // schemeName: "$schemeDetails.schemeName",
         schemeName: {
@@ -270,14 +278,11 @@ module.exports.getAssignedScheme = asyncErrorHandler(async (req, res) => {
             { $ifNull: ["$schemeDetails.period", ""] },
           ],
         },
-        branchName: "$branchDetails.branchName",
-        branchLocation: "$branchDetails.state",
         scheme_id: 1,
-        bo_id: 1,
-        assignQty: 1,
-        status: 1,
+        procurement: 1,
+        status: 1
       },
-    },
+    }
   ];
 
   if (paginate == 1) {
@@ -307,6 +312,7 @@ module.exports.getAssignedScheme = asyncErrorHandler(async (req, res) => {
       status: firstRow.status,
       schemeId: firstRow.schemeId,
       schemeName: firstRow.schemeName,
+      procurementTarget: firstRow.procurement,
     };
   }
 
@@ -363,95 +369,90 @@ module.exports.getslaByBo = async (req, res) => {
   const { bo_id, page = 1, limit = 10, skip = 0, paginate = 1, sortBy, search = '', isExport = 0 } = req.query;
 
   // Initialize matchQuery
-  let matchQuery = { bo_id: new mongoose.Types.ObjectId(bo_id) };
+
+  let matchQuery = {
+    bo_id: new mongoose.Types.ObjectId(bo_id),
+    sla_id: { $exists: true, $ne: null }
+  };
 
   // Validate ObjectId
   if (!mongoose.Types.ObjectId.isValid(bo_id)) {
-      return res.status(400).json({ message: "Invalid BO ID" });
+    return res.status(400).json({ message: "Invalid BO ID" });
   }
 
   let aggregationPipeline = [
-      { $match: matchQuery },
-      {
-          $lookup: {
-              from: 'branches',
-              localField: 'bo_id',
-              foreignField: '_id',
-              as: 'branchDetails',
-          },
-      },
-      { $unwind: { path: '$branchDetails', preserveNullAndEmptyArrays: true } },
-      {
-          $lookup: {
-              from: "schemes", // Adjust this to your actual collection name for branches
-              localField: "scheme_id",
-              foreignField: "_id",
-              as: "schemeDetails"
-          }
-      },
-      { $unwind: { path: "$schemeDetails", preserveNullAndEmptyArrays: true } },
-      {
-          $project: {
-              _id: 1,
-              schemeId: '$schemeDetails.schemeId',
-              // schemeName: '$schemeDetails.schemeName',
-              schemeName: {
-                  $concat: [
-                      "$schemeDetails.schemeName", "",
-                      { $ifNull: ["$schemeDetails.commodityDetails.name", ""] }, "",
-                      { $ifNull: ["$schemeDetails.season", ""] }, "",
-                      { $ifNull: ["$schemeDetails.period", ""] }
-                  ]
-              },
-              branchName: '$branchDetails.branchName',
-              assignQty: "$assignQty",
-              procurement: "$schemeDetails.procurement", 
-              createdOn: '$createdAt'
-          }
+    { $match: matchQuery },
+    {
+      $lookup: {
+        from: "schemes", // Adjust this to your actual collection name for branches
+        localField: "scheme_id",
+        foreignField: "_id",
+        as: "schemeDetails"
       }
+    },
+    // { $unwind: { path: "$schemeDetails", preserveNullAndEmptyArrays: true } },
+    { $unwind: { path: "$schemeDetails" } },
+    {
+      $lookup: {
+        from: "slas", // Adjust this to your actual collection name for branches
+        localField: "sla_id",
+        foreignField: "_id",
+        as: "slaDetails"
+      }
+    },
+    { $unwind: { path: "$slaDetails", preserveNullAndEmptyArrays: true } },
+    {
+      $project: {
+        _id: 1,
+        slaName: '$slaDetails.basic_details.name',
+        slaId: '$slaDetails.slaId',
+        assignQty: "$assignQty",
+        targetAchieved: '$schemeDetails.procurement'
+      }
+    }
   ];
   if (paginate == 1) {
-      aggregationPipeline.push(
-          { $sort: { [sortBy || 'createdAt']: -1, _id: -1 } }, // Secondary sort by _id for stability
-          { $skip: parseInt(skip) },
-          { $limit: parseInt(limit) }
-      );
+    aggregationPipeline.push(
+      { $sort: { [sortBy || 'createdAt']: -1, _id: -1 } }, // Secondary sort by _id for stability
+      { $skip: parseInt(skip) },
+      { $limit: parseInt(limit) }
+    );
   } else {
-      aggregationPipeline.push({ $sort: { [sortBy || 'createdAt']: -1, _id: -1 } },);
+    aggregationPipeline.push({ $sort: { [sortBy || 'createdAt']: -1, _id: -1 } },);
   }
 
   const rows = await SchemeAssign.aggregate(aggregationPipeline);
   const countPipeline = [
-      { $match: matchQuery }, 
-      { $count: "total" }
+    { $match: matchQuery },
+    { $count: "total" }
   ];
   const countResult = await SchemeAssign.aggregate(countPipeline);
   const count = countResult[0]?.total || 0;
   const records = { rows, count };
   if (paginate == 1) {
-      records.page = parseInt(page);
-      records.limit = parseInt(limit);
-      records.pages = limit != 0 ? Math.ceil(count / limit) : 0;
+    records.page = parseInt(page);
+    records.limit = parseInt(limit);
+    records.pages = limit != 0 ? Math.ceil(count / limit) : 0;
   }
   if (isExport == 1) {
-      const record = rows.map((item) => {
-          return {
-              "Scheme Id": item?.schemeId || "NA",
-              "schemeName": item?.schemeName || "NA",
-              "branchName": item?.branchName || "NA",
-          };
+    const record = rows.map((item) => {
+      return {
+        "Scheme Id": item?.schemeId || "NA",
+        "schemeName": item?.schemeName || "NA",
+        "branchName": item?.branchName || "NA",
+      };
+    });
+    if (record.length > 0) {
+      dumpJSONToExcel(req, res, {
+        data: record,
+        fileName: `Scheme-record.xlsx`,
+        worksheetName: `Scheme-record`
       });
-      if (record.length > 0) {
-          dumpJSONToExcel(req, res, {
-              data: record,
-              fileName: `Scheme-record.xlsx`,
-              worksheetName: `Scheme-record`
-          });
-      } else {
-          return res.status(200).send(new serviceResponse({ status: 200, data: records, message: _response_message.notFound("Scheme Assign") }));
-      }
+    } else {
+      return res.status(200).send(new serviceResponse({ status: 200, data: records, message: _response_message.notFound("Scheme Assign") }));
+    }
   } else {
-      return res.status(200).send(new serviceResponse({ status: 200, data: records, message: _response_message.found("Scheme Assign") }));
+    return res.status(200).send(new serviceResponse({ status: 200, data: records, message: _response_message.found("Scheme Assign") }));
   }
 }
 
