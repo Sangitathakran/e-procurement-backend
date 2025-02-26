8c4b72fb318843bcee0843aba56d1bd66ae4a0cb
raman15 <raman@navankur.org>
2025-02-26 17:45:56 +0530

    adding auth middleware in sla route in bo dahsboard

--- src/v1/modules/branch-office/Routes.js
+++ src/v1/modules/branch-office/Routes.js
@@ -8,6 +8,7 @@ const { dashboardRoutes } = require("./dashboard/Routes");
 const { assignSchemeRoutes } = require("./assignScheme/Routes");
 const { slaRoute } = require("./sla-management/Routes");
 const branchOfficeoRoutes = express.Router();
+const { Auth } = require("@src/v1/middlewares/jwt");
 
 
 branchOfficeoRoutes.use("/payment", paymentRoutes);
@@ -16,6 +17,6 @@ branchOfficeoRoutes.use("/center", procurementCenterRoutes);
 branchOfficeoRoutes.use("/req", requirementRoutes);
 branchOfficeoRoutes.use("/dashboard", dashboardRoutes);
 branchOfficeoRoutes.use("/assignScheme", assignSchemeRoutes);
-branchOfficeoRoutes.use("/sla", slaRoute);
+branchOfficeoRoutes.use("/sla", Auth, slaRoute);
 
 module.exports = { branchOfficeoRoutes };
