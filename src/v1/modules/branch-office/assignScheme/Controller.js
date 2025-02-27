01c79e0ae9a5fc41822d1ad0ef8e015b7de56d7a
Raj kapoor <raj@navankur.org>
2025-02-27 16:10:02 +0530

    branch office filter for scheme and warehouse

--- src/v1/modules/branch-office/assignScheme/Controller.js
+++ src/v1/modules/branch-office/assignScheme/Controller.js
@@ -13,7 +13,7 @@ const { mongoose } = require("mongoose");
 
 module.exports.getAssignedScheme = asyncErrorHandler(async (req, res) => {
   try {
-    const { page = 1, limit = 10, skip = 0, paginate = 1, sortBy, search = '', schemeName, status, isExport = 0 } = req.query;
+    const { page = 1, limit = 10, skip = 0, paginate = 1, sortBy, search = '', schemeName, status, commodity, season, isExport = 0 } = req.query;
 
     const { user_id } = req;
 
@@ -25,9 +25,27 @@ module.exports.getAssignedScheme = asyncErrorHandler(async (req, res) => {
     if (search) {
       matchQuery.schemeId = { $regex: search, $options: "i" };
     }
+    if (schemeName) {
+      matchQuery["schemeDetails.schemeName"] = { $regex: schemeName, $options: "i" };
+    }
+
+    console.log("schemeName", schemeName)
+    // Commodity filter
+    if (commodity) {
+      matchQuery["commodityDetails.name"] = { $regex: commodity, $options: "i" };
+    }
+
+    // Season filter
+    if (season) {
+      matchQuery["schemeDetails.season"] = { $regex: season, $options: "i" };
+    }
+
+    // Status filter
+    if (status) {
+      matchQuery["schemeDetails.status"] = status;
+    }
 
     let aggregationPipeline = [
-      { $match: matchQuery },
       {
         $lookup: {
           from: "schemes",
@@ -46,6 +64,7 @@ module.exports.getAssignedScheme = asyncErrorHandler(async (req, res) => {
         },
       },
       { $unwind: { path: "$commodityDetails", preserveNullAndEmptyArrays: true } },
+      { $match: matchQuery },
       {
         $project: {
           _id: 1,
