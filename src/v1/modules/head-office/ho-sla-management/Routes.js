1f2fb17a42343b60d622036263303759bcc28d44
raman15 <raman@navankur.org>
2025-02-27 19:59:37 +0530

    assign scheme in ho

--- src/v1/modules/head-office/ho-sla-management/Routes.js
+++ src/v1/modules/head-office/ho-sla-management/Routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { createSLA, getSLAList, deleteSLA, updateSLA, getSLAById, updateSLAStatus, addSchemeToSLA, } = require("./Controller");
+const { createSLA, getSLAList, deleteSLA, updateSLA, getSLAById, updateSLAStatus, addSchemeToSLA, schemeAssign, getAssignedScheme, } = require("./Controller");
 const { Auth } = require("@src/v1/middlewares/jwt");
 
 const slaRoute = express.Router();
 
-// slaRoute.post("/schemeAssign", Auth, schemeAssign);
-// slaRoute.get("/getAssignedScheme", Auth, getAssignedScheme);
+slaRoute.post("/schemeAssign", schemeAssign);
+slaRoute.get("/getAssignedScheme", getAssignedScheme);
 
 slaRoute.post("/", createSLA);
 slaRoute.get("/", getSLAList);
