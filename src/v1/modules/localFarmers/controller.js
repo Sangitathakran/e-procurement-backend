1bd977b39e5c6c6031812c3af65e21418c1a21d3
Pradeep Yadav <pradeep@navankur.org>
2025-04-02 14:10:06 +0530

    modify last created  farmers index

--- src/v1/modules/localFarmers/controller.js
+++ src/v1/modules/localFarmers/controller.js
@@ -19,7 +19,7 @@ module.exports.saveExternalFarmerData = async (req, res) => {
     localFarmersLogger.warn("dates field is required");
     return res.json({ success: false, message: "dates field is required " });
   }
-  let farmersData = isExport ? Farmersdata : [];
+  let farmersData = isExport ? Farmersdata.slice(17496) : [];
   // return res.json( { data: farmersData.length} );
   const api_endpoint = isExport ? [] : dates.map(
     (date) => `${process.env.HARYANA_F_API_ENDPOINT}?date=${date}`
