d52f784aa77ac5951ea7af6031256c62eff15f70
raman15 <Raman@navankur.org>
2025-04-10 13:33:55 +0530

    some changes in whr image

--- src/v1/modules/head-office/payment/Controller.js
+++ src/v1/modules/head-office/payment/Controller.js
@@ -1729,14 +1729,7 @@ module.exports.batchList = async (req, res) => {
       associateOffer_id: new mongoose.Types.ObjectId(associateOffer_id),
       bo_approve_status: _paymentApproval.approved,
       ho_approve_status: batch_status == _paymentApproval.pending ? _paymentApproval.pending : _paymentApproval.approved,
-      ...(search ? 
-        {
-          $or: [
-            { batchId: { $regex: search, $options: 'i' } },
-             { whrNo: { $regex: search, $options: 'i' } }
-          ]
-        }
-        :  {}) // Search functionality
+      ...(search ? { order_no: { $regex: search, $options: 'i' } } : {}) // Search functionality
     };
 
     const records = { count: 0 };
@@ -3532,7 +3525,7 @@ module.exports.proceedToPayPayment = async (req, res) => {
           "batches.ho_approve_status": _paymentApproval.approved ? _paymentApproval.approved : { $ne: _paymentApproval.pending },
           "batches.payment.payment_status": paymentStatusCondition || _paymentstatus.pending
         }
-      },     
+      },
       {
         $addFields: {
           qtyPurchased: {
@@ -3581,6 +3574,7 @@ module.exports.proceedToPayPayment = async (req, res) => {
 
     let response = { count: 0 };
     response.rows = await RequestModel.aggregate(aggregationPipeline);
+    console.log(aggregationPipeline);
     const countResult = await RequestModel.aggregate([...aggregationPipeline.slice(0, -2), { $count: "count" }]);
     response.count = countResult?.[0]?.count ?? 0;
 
@@ -3604,6 +3598,7 @@ module.exports.proceedToPayPayment = async (req, res) => {
 };
 
 module.exports.proceedToPayBatchList = async (req, res) => {
+
   try {
     const { page, limit, skip, paginate = 1, sortBy, search = '', associateOffer_id, req_id, payment_status, isExport = 0 } = req.query
 
@@ -3762,7 +3757,6 @@ module.exports.proceedToPayBatchList = async (req, res) => {
           qtyPurchased: "$qty",
           amountProposed: "$goodsPrice",
           associateName: "$users.basic_details.associate_details.associate_name",
-          organisationName: "$users.basic_details.associate_details.organization_name",
           // whrNo: "12345",
           // whrReciept: "whrReciept.jpg",
           whrNo: "$final_quality_check.whr_receipt",
