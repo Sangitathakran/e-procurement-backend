2c20e897d76e35edcda4695513673812e44547ba
Shivanjal Dwivedi <Shivanjal@navankur.org>
2025-04-24 11:04:58 +0530

    Update a export in  Procurement Center sections

--- src/v1/modules/associate/procurement_center/Controller.js
+++ src/v1/modules/associate/procurement_center/Controller.js
@@ -441,18 +441,21 @@ module.exports.getHoProcurementCenter = async (req, res) => {
     if (isExport == 1) {
       const record = records.rows.map((item) => {
         return {
-          "Address Line 1": item?.address?.line1 || "NA",
-          "Address Line 2": item?.address?.line2 || "NA",
-          Country: item?.address?.country || "NA",
+          "CENTER ID": item?.center_code || "NA",
+          "CENTER TYPE": item?.center_code || "NA",
+          "CENTER NAME": item?.center_code || "NA",
+          CONTACT: item?.point_of_contact?.mobile || "NA",
+          EMAIL: item?.point_of_contact?.email || "NA",
           State: item?.address?.country || "NA",
-          District: item?.address?.district || "NA",
           City: item?.address?.city || "NA",
-          "PIN Code": item?.address?.postalCode || "NA",
-          Name: item?.point_of_contact?.name || "NA",
-          Email: item?.point_of_contact?.email || "NA",
-          Mobile: item?.point_of_contact?.mobile || "NA",
-          Designation: item?.point_of_contact?.designation || "NA",
-          "Aadhar Number": item?.point_of_contact?.aadhar_number || "NA",
+          "POINT OF CONTACT": item?.point_of_contact?.name || "NA",
+          // "Address Line 1": item?.address?.line1 || "NA",
+          // "Address Line 2": item?.address?.line2 || "NA",
+          // Country: item?.address?.country || "NA",
+          // District: item?.address?.district || "NA",
+          // "PIN Code": item?.address?.postalCode || "NA",
+          // Designation: item?.point_of_contact?.designation || "NA",
+          // "Aadhar Number": item?.point_of_contact?.aadhar_number || "NA",
         };
       });
 
