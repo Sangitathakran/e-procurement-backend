b5f05192b260e2e5466e8b8455250a732520ee14
dileep <dileep@radiantinfonet.com>
2025-04-29 12:49:19 +0530

    commited the code

--- app.js
+++ app.js
@@ -59,10 +59,10 @@ app.get(
   })
 );
 
-const used = process.memoryUsage();
-for (let key in used) {
-  console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
-}
+// const used = process.memoryUsage();
+// for (let key in used) {
+//   console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
+// }
 
 /* Handle errors */
 //app.use(handleCatchError)
