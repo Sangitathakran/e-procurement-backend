41274a2f5c8a6424fbdc78b2a7ee48db9b5dc11e
Pradeep Yadav <pradeep@navankur.org>
2025-05-01 17:33:43 +0530

    optimized successfull payment api and farmer count statewise api

--- src/v1/modules/head-office/payment/Routes.js
+++ src/v1/modules/head-office/payment/Routes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const { payment, associateOrders, batchList, batchApprove, qcReport, lot_list, approvedBatchList, payFarmers,
     updatePaymentByOrderId, sendOTP, verifyOTPProceed, verifyOTPApproval, paymentLogsHistory, proceedToPayPayment, proceedToPayBatchList, 
     paymentWithoutAgreegation, batchListWithoutAggregation, proceedToPaybatchListWithoutAggregation,
-    batchListWOAggregation} = require("./Controller");
+    batchListWOAggregation,
+    getTotalSuccessfulPaidAmount,
+    proceedToPayPaymentWOAggregation} = require("./Controller");
 const { Auth } = require("@src/v1/middlewares/jwt")
 
 const paymentRoutes = express.Router();
@@ -20,7 +22,9 @@ paymentRoutes.get("/approved-batch-list", Auth, approvedBatchList);
 paymentRoutes.post("/pay-farmers", Auth, payFarmers)
 paymentRoutes.put("/update-payment-status", Auth, updatePaymentByOrderId)
 
-paymentRoutes.get("/proceed-to-pay", Auth, proceedToPayPayment);
+//paymentRoutes.get("/proceed-to-pay", Auth, proceedToPayPayment);
+paymentRoutes.get("/get-paidAmount", Auth, getTotalSuccessfulPaidAmount);
+
 // paymentRoutes.get("/proceed-to-pay-batch-list", Auth, proceedToPayBatchList);
 paymentRoutes.get("/proceed-to-pay-batch-list", Auth, proceedToPaybatchListWithoutAggregation);
 paymentRoutes.get("/payment-logs", Auth, paymentLogsHistory);
@@ -44,6 +48,8 @@ paymentRoutes.put("/bill-reject", Auth, hoBillRejection)
 
 // ****************************** API WITHOUT AGGREGATION   *******************************
 //paymentRoutes.get("/", Auth, paymentWithoutAgreegation);
+paymentRoutes.get("/proceed-to-pay", Auth, proceedToPayPaymentWOAggregation);
+
 
 
 
