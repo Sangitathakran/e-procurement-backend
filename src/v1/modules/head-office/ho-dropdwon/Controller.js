04fa3e16f14ffe20a61f473a43715e0b01490ac5
Shivanjal Dwivedi <Shivanjal@navankur.org>
2025-05-12 10:18:09 +0530

    Update a scheme end points for dropdown

--- src/v1/modules/head-office/ho-dropdwon/Controller.js
+++ src/v1/modules/head-office/ho-dropdwon/Controller.js
@@ -4,13 +4,13 @@ const { SchemeAssign } = require("@src/v1/models/master/SchemeAssign");
 module.exports.HoAllScheme = async (req, res) => {
   try {
     const { user_id, portalId } = req;
-   
-     const portalObjectId = new mongoose.Types.ObjectId(portalId);
+
+    const portalObjectId = new mongoose.Types.ObjectId(portalId);
     const data = await SchemeAssign.aggregate([
       {
         $match: {
           deletedAt: null,
-          ho_id : portalObjectId
+          ho_id: portalObjectId,
         },
       },
       {
@@ -42,31 +42,20 @@ module.exports.HoAllScheme = async (req, res) => {
         },
       },
       {
-        $group: {
-            _id : 0,
-          scheme: {
-            $addToSet: {
-              id: "$scheme_id",
-              name: {
-                $concat: [
-                  { $ifNull: ["$schemeDetails.schemeName", ""] },
-                  "  ",
-                  { $ifNull: ["$commodityDetails.name", ""] },
-                  { $ifNull: ["$commodityDetails.name", ""] },
-                  "  ",
-                  { $ifNull: ["$schemeDetails.season", ""] },
-                  "  ",
-                  { $ifNull: ["$schemeDetails.period", ""] },
-                ],
-              },
-            },
-          },
-        },
-      },
-      {
         $project: {
-          _id: 0,
-          scheme: 1,
+          originalSchemeName: "$schemeName",
+          schemeName: {
+            $concat: [
+              { $ifNull: ["$schemeDetails.schemeName", ""] },
+              " ",
+              { $ifNull: ["$commodityDetails.name", ""] },
+              " ",
+              { $ifNull: ["$schemeDetails.season", ""] },
+              " ",
+              { $ifNull: ["$schemeDetails.period", ""] },
+            ],
+          },
+          procurement: 1,
         },
       },
     ]);
@@ -75,11 +64,8 @@ module.exports.HoAllScheme = async (req, res) => {
       status: 200,
       data: data,
     });
-  } catch (error) {
-    console.error("Aggregation Error:", error);
-    return res.status(500).json({
-      status: 500,
-      message: "Internal Server Error",
-    });
+  } catch (err) {
+    console.log("ERROR: ", err);
+    return sendResponse({ status: 500, message: err.message });
   }
 };
