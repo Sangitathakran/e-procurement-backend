05739a2fe278a6c51be99704778b500ea7408b2d
Neeraj Maurya <neeraj75725612@gmail.com>
2025-05-20 13:47:44 +0530

    code refactor

--- src/v1/modules/upag/stock/Routes.js
+++ src/v1/modules/upag/stock/Routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { getStockData } = require("./Controllers");
+const { authMiddleware } = require("../auth/Controllers");
 const stockRoutes = express.Router()
 
 stockRoutes.get("/get-stock",authMiddleware,getStockData)
