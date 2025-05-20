d85816d6524cf9658eefc41b5206109750cc5672
Neeraj Maurya <neeraj75725612@gmail.com>
2025-05-20 21:56:50 +0530

    Fixed distiller auth issue

--- src/v1/modules/distiller/utils/verifyDistiller.js
+++ src/v1/modules/distiller/utils/verifyDistiller.js
@@ -31,8 +31,7 @@ exports.verifyDistiller = asyncErrorHandler(async (req, res, next) => {
         }
 
         // const userExist = await Distiller.findOne({ _id: decodedToken.organization_id })
-         const userExist = await Distiller.findOne({ _id: decodedToken.user_id })
-
+         const userExist = await Distiller.findOne({ _id: decodedToken.user.portalId })
         if (!userExist) {
             return res.status(200).send(new serviceResponse({ status: 401, errors: [{ message: _response_message.notFound("User") }] }));
         }
