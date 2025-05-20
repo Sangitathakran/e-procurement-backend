b76232cb51d8c423acd2f80dd90aaac9ea8f747e
Neeraj Maurya <neeraj75725612@gmail.com>
2025-05-20 12:14:43 +0530

    upag api service done

--- src/v1/modules/upag/procurement/Routes.js
+++ src/v1/modules/upag/procurement/Routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { getProcurementData } = require("./Controllers");
+const { authMiddleware } = require("../auth/Controllers");
 const procurementRoutes = express.Router()
 
-procurementRoutes.get("/get-procurement",getProcurementData)
+procurementRoutes.get("/get-procurement",authMiddleware,getProcurementData)
 module.exports = { procurementRoutes };
 
