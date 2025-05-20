66763a6e14dd8c585e2d0012f6312f0f29e816b9
Neeraj Maurya <neeraj75725612@gmail.com>
2025-05-20 13:47:01 +0530

    add token

--- src/v1/modules/upag/stock/Routes.js
+++ src/v1/modules/upag/stock/Routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { getStockData } = require("./Controllers");
 const stockRoutes = express.Router()
 
-stockRoutes.get("/get-stock",getStockData)
+stockRoutes.get("/get-stock",authMiddleware,getStockData)
 module.exports = { stockRoutes };
 
