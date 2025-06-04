e8f413a861f2694a1d91d07f17c98d98273c67d3
Raj kapoor <raj@navankur.org>
2025-06-04 16:37:37 +0530

    chanes

--- src/v1/modules/associate/order/Controller.js
+++ src/v1/modules/associate/order/Controller.js
@@ -466,19 +466,18 @@ module.exports.trackDeliveryByBatchId = async (req, res) => {
             .select({ dispatched: 1, intransit: 1, status: 1, delivered: 1 })
             .populate({
                 path: 'req_id', select: 'product address'
-            });
+            }).lean();
 
         if (!record) {
             return res.status(400).send(new serviceResponse({ status: 400, errors: [{ message: _response_message.notFound("Track order") }] }))
         }
-        const data = record.toObject();
-        if (data.req_id?.address?.deliveryLocation) {
-            data.intransit = {
-                ...data.intransit,
-                deliveryLocation: data.req_id.address.deliveryLocation
+        if (record.req_id?.address?.deliveryLocation) {
+            record.intransit = {
+                ...record.intransit,
+                deliveryLocation: record.req_id.address.deliveryLocation
             };
         }
-        return res.status(200).send(new serviceResponse({ status: 200, data: data, message: _response_message.found("Track order") }));
+        return res.status(200).send(new serviceResponse({ status: 200, data: record, message: _response_message.found("Track order") }));
     } catch (error) {
         _handleCatchErrors(error, res);
     }
