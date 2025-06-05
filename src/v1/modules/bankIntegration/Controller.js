dcf02d60d9034e9b2a8512206bb5b593313874ea
shyam47 <shyam@navankur.org>
2025-06-05 16:55:06 +0530

    reslove error2

--- src/v1/modules/bankIntegration/Controller.js
+++ src/v1/modules/bankIntegration/Controller.js
@@ -69,7 +69,7 @@ module.exports.sendRequest = async (req, res) => {
     // const decrypted = ccav.decrypt(encRequest, keyBase64, ivBase64);
     // console.log("decrypted response myEnc=>", decrypted);
 
-    return res.json({ paymentUrl, status: 200 });
+    return res.json({ accessCode:accessCode,encRequest:encRequest, status: 200 });
     // return res.redirect(paymentUrl);
   } catch (error) {
     console.error("Encryption Error:", error);
