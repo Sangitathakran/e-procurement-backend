f8cf8603a2f90ba9ca2663660fac0b20d56217d4
Sangita <sangita@navankur.org>
2025-06-12 18:57:41 +0530

    po raised

--- src/v1/modules/nccf/ministry/dashboard/Routes.js
+++ src/v1/modules/nccf/ministry/dashboard/Routes.js
@@ -1,7 +1,7 @@
 const ministrydashboardRoutes = require("express").Router();
 const { Auth } = require("@src/v1/middlewares/jwt")
 const { _middleware } = require("@src/v1/utils/constants/messages");
-const { getDashboardStats, monthlyLiftedTrends, getMonthlyPayments, stateWiseQuantity, warehouseList } = require("./Controller");
+const { getDashboardStats, monthlyLiftedTrends, getMonthlyPayments, stateWiseQuantity, warehouseList, poRaised } = require("./Controller");
 
 
 ministrydashboardRoutes.get("/", Auth, getDashboardStats);
@@ -9,5 +9,6 @@ ministrydashboardRoutes.get("/monthlyLiftedTrends", Auth, monthlyLiftedTrends);
 ministrydashboardRoutes.get("/monthlyPaymentRecieved", getMonthlyPayments);
 ministrydashboardRoutes.get("/stateWiseQuantity", stateWiseQuantity);
 ministrydashboardRoutes.get("/warehouseList", warehouseList);
+ministrydashboardRoutes.get("/poRaised", poRaised);
 
 module.exports = { ministrydashboardRoutes }; 
