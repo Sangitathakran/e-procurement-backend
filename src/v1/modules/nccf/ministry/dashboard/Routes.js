e9940a45fd17f1bf86e4adb740d0a1f0cae4948c
Sangita <sangita@navankur.org>
2025-06-16 18:17:11 +0530

    state wise analysis API

--- src/v1/modules/nccf/ministry/dashboard/Routes.js
+++ src/v1/modules/nccf/ministry/dashboard/Routes.js
@@ -2,7 +2,7 @@ const ministrydashboardRoutes = require("express").Router();
 const { Auth } = require("@src/v1/middlewares/jwt")
 const { _middleware } = require("@src/v1/utils/constants/messages");
 const { getDashboardStats, monthlyLiftedTrends, getMonthlyPayments, stateWiseQuantity, warehouseList, poRaised,
-    ongoingOrders, getStateWishProjection
+    ongoingOrders, stateWiseAnalysis, getStateWishProjection
 } = require("./Controller");
 
 
@@ -13,6 +13,7 @@ ministrydashboardRoutes.get("/stateWiseQuantity", stateWiseQuantity);
 ministrydashboardRoutes.get("/warehouseList", warehouseList);
 ministrydashboardRoutes.get("/poRaised", poRaised);
 ministrydashboardRoutes.get("/ongoingOrders", ongoingOrders);
+ministrydashboardRoutes.get("/stateWiseAnalysis", stateWiseAnalysis);
 ministrydashboardRoutes.get("/getCenterProjections", Auth, getStateWishProjection);
 
 module.exports = { ministrydashboardRoutes }; 
