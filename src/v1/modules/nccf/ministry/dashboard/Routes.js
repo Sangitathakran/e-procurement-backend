c6ad54033cdb2bb9908aa3f1eeb998a993ce48c9
Sangita <sangita@navankur.org>
2025-06-19 17:10:10 +0530

    stateWiseProcurementQuantity stateWiseLiftingQuantity API

--- src/v1/modules/nccf/ministry/dashboard/Routes.js
+++ src/v1/modules/nccf/ministry/dashboard/Routes.js
@@ -1,8 +1,9 @@
 const ministrydashboardRoutes = require("express").Router();
 const { Auth } = require("@src/v1/middlewares/jwt")
 const { _middleware } = require("@src/v1/utils/constants/messages");
-const { getDashboardStats, monthlyLiftedTrends, getMonthlyPayments, stateWiseQuantity, warehouseList, poRaised,
-    ongoingOrders, stateWiseAnalysis, getStateWishProjection, paymentWithTenPercant, paymentWithHundredPercant
+const { getDashboardStats, monthlyLiftedTrends, getMonthlyPayments, stateWiseQuantity, stateWiseProcurementQuantity,
+     stateWiseLiftingQuantity, warehouseList, poRaised, ongoingOrders, stateWiseAnalysis, getStateWishProjection, 
+     paymentWithTenPercant, paymentWithHundredPercant
 } = require("./Controller");
 
 
@@ -10,6 +11,8 @@ ministrydashboardRoutes.get("/", Auth, getDashboardStats);
 ministrydashboardRoutes.get("/monthlyLiftedTrends", Auth, monthlyLiftedTrends);
 ministrydashboardRoutes.get("/monthlyPaymentRecieved", Auth, getMonthlyPayments);
 ministrydashboardRoutes.get("/stateWiseQuantity", Auth, stateWiseQuantity);
+ministrydashboardRoutes.get("/stateWiseProcurementQuantity", Auth, stateWiseProcurementQuantity);
+ministrydashboardRoutes.get("/stateWiseLiftingQuantity", Auth, stateWiseLiftingQuantity);
 ministrydashboardRoutes.get("/warehouseList", Auth, warehouseList);
 ministrydashboardRoutes.get("/poRaised", Auth, poRaised);
 ministrydashboardRoutes.get("/ongoingOrders", Auth, ongoingOrders);
