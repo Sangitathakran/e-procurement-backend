37293dd392ede6c2162cfc20d7380cb8ffdcdb44
Sangita <sangita@navankur.org>
2025-06-23 18:44:07 +0530

    stats api changes and OMC Report changes

--- src/v1/modules/nccf/ministry/dashboard/Routes.js
+++ src/v1/modules/nccf/ministry/dashboard/Routes.js
@@ -1,13 +1,14 @@
 const ministrydashboardRoutes = require("express").Router();
 const { Auth } = require("@src/v1/middlewares/jwt")
 const { _middleware } = require("@src/v1/utils/constants/messages");
-const { getDashboardStats, monthlyLiftedTrends, getMonthlyPayments, stateWiseQuantity, stateWiseProcurementQuantity,
+const { getDashboardStats, filterWiseDashboardStats, monthlyLiftedTrends, getMonthlyPayments, stateWiseQuantity, stateWiseProcurementQuantity,
      stateWiseLiftingQuantity, warehouseList, poRaised, ongoingOrders, stateWiseAnalysis, getStateWishProjection, 
      paymentWithTenPercant, paymentWithHundredPercant
 } = require("./Controller");
 
 
 ministrydashboardRoutes.get("/", Auth, getDashboardStats);
+ministrydashboardRoutes.get("/filterWiseDashboardStats", Auth, filterWiseDashboardStats);
 ministrydashboardRoutes.get("/monthlyLiftedTrends", Auth, monthlyLiftedTrends);
 ministrydashboardRoutes.get("/monthlyPaymentRecieved", Auth, getMonthlyPayments);
 ministrydashboardRoutes.get("/stateWiseQuantity", Auth, stateWiseQuantity);
