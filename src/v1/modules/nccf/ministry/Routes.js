e91bd378903a1398e8dadbfcdd68b147e706ae2e
Sangita <sangita@navankur.org>
2025-06-26 11:47:14 +0530

    extra code removed

--- src/v1/modules/nccf/ministry/Routes.js
+++ src/v1/modules/nccf/ministry/Routes.js
@@ -1,6 +1,5 @@
 console.log("Loading ministry routes...");
 const express = require("express");
-const { distillerAuthRoutes } = require("./auth/Routes");
 const {dropDownRoutes} = require("./dropDown/Routes")
 const { ministrydashboardRoutes } = require("./dashboard/Routes");
 const { reportRoutes } = require("./report/Routes");
@@ -8,10 +7,8 @@ const { reportRoutes } = require("./report/Routes");
 
 const ministryRoutes = express.Router();
 
-ministryRoutes.use("/auth", distillerAuthRoutes);
 ministryRoutes.use("/dropDown", dropDownRoutes);
 ministryRoutes.use("/dashboard", ministrydashboardRoutes);
 ministryRoutes.use("/report", reportRoutes);
-
 
 module.exports = { ministryRoutes };
