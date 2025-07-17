958cd5a66eb8c63ea27b18cef599429080276a86
Pradeep Yadav <pradeep@navankur.org>
2025-07-17 15:24:29 +0530

    added static response for testing

--- src/v1/middlewares/on-grid.js
+++ src/v1/middlewares/on-grid.js
@@ -1,14 +1,35 @@
 const { NODE_ENV } = require("@config/index");
 
 module.exports.onGridMiddleware = async (req, res, next) => {
+ // console.log(req.body);
   // Only allow in production
-  if (NODE_ENV === 'production') {
+  if (NODE_ENV === "production") {
     return next(); // Allow access
   }
 
+  if (req.url == "/verify") {
+    return res.json({
+      status: 200,
+      data: {
+        bank_data: {
+          is_verified: true,
+          account_holder_name: "GENDALAL GURJAR",
+          account_no: "951118210006876",
+          bank_name: "BANK OF INDIA",
+          branch_name: "BORGAON BUZURG",
+          ifsc_code: "BKID0009511",
+        },
+      },
+      message: "This is a static response only for testing.",
+      event: {},
+      errorCode: "",
+      errors: [],
+      version: "1.0",
+    });
+  }
   // Block in dev/test
   return res.status(400).json({
     status: 400,
-    message: 'ON-GRID API can only be used in the production environment!',
+    message: "ON-GRID API can only be used in the production environment!",
   });
 };
