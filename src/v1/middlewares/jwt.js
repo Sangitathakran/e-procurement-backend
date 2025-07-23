241619658734d66ba0b18a4a1670450c41ad79a8
shyam47 <shyam@navankur.org>
2025-07-24 00:20:37 +0530

    changes order jwt

--- src/v1/middlewares/jwt.js
+++ src/v1/middlewares/jwt.js
@@ -28,7 +28,7 @@ const authorizeRoles = (...allowedRoles) => {
     if (!user || !allowedRoles.includes(user?.user_type)) {
       return sendResponse({
         res,
-        status: 403,
+        status: 404,
         data: [],
         message: "You do not have permission to access this resource",
         errors: _auth_module.forbidden,
