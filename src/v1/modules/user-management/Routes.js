15a1e31efed70df10860cce0a4d27bc51157a48c
shyam47 <shyam@navankur.org>
2025-07-24 09:37:45 +0530

    added new chages

--- src/v1/modules/user-management/Routes.js
+++ src/v1/modules/user-management/Routes.js
@@ -6,9 +6,9 @@ const { _userType } = require("@src/v1/utils/constants/index")
 const { createUserRole, getFeatures , editUserRolePage, editUserRole, getUserRoles, getUserRoleList } = require("./Controller")
 
 userManagementRoutes.get('/getFeatures/:type', Auth , getFeatures )
-userManagementRoutes.post('/createUserRole' ,authenticateUser,authorizeRoles(_userType.admin), Auth, createUserRole )
-userManagementRoutes.get('/editUserRolePage/:id',authenticateUser,authorizeRoles(_userType.admin) ,Auth, editUserRolePage )
-userManagementRoutes.put('/editUserRole',authenticateUser,authorizeRoles(_userType.admin) , Auth, editUserRole )
+userManagementRoutes.post('/createUserRole' ,authenticateUser,authorizeRoles(_userType.ho ,_userType.warehouse ,_userType.admin ,_userType.bo,_userType.nccf,_userType.distiller), Auth, createUserRole )
+userManagementRoutes.get('/editUserRolePage/:id',authenticateUser,authorizeRoles(_userType.ho ,_userType.warehouse ,_userType.admin ,_userType.bo,_userType.nccf,_userType.distiller) ,Auth, editUserRolePage )
+userManagementRoutes.put('/editUserRole',authenticateUser,authorizeRoles(_userType.ho ,_userType.warehouse ,_userType.admin ,_userType.bo,_userType.nccf,_userType.distiller) , Auth, editUserRole )
 userManagementRoutes.get('/getUserRoles', Auth , getUserRoles )
 userManagementRoutes.get('/getUserRoleList', Auth, getUserRoleList)
 
@@ -16,12 +16,12 @@ userManagementRoutes.get('/getUserRoleList', Auth, getUserRoleList)
 //user routes 
 const { createUser, getUserPermission, editUser, toggleStatus, getUsersByUser, getSingleUser } = require("./Controller")
 
-userManagementRoutes.post('/createUser',authenticateUser,authorizeRoles(_userType.admin), Auth, createUser )
+userManagementRoutes.post('/createUser',authenticateUser,authorizeRoles(_userType.ho ,_userType.warehouse ,_userType.admin ,_userType.bo,_userType.nccf,_userType.distiller), Auth, createUser )
 userManagementRoutes.get('/getUserPermission',Auth ,  getUserPermission )
-userManagementRoutes.put('/editUser',authenticateUser,authorizeRoles(_userType.admin), Auth , editUser )
-userManagementRoutes.put('/toggleStatus/:id',authenticateUser,authorizeRoles(_userType.admin), Auth , toggleStatus )
+userManagementRoutes.put('/editUser',authenticateUser,authorizeRoles(_userType.ho ,_userType.warehouse ,_userType.admin ,_userType.bo,_userType.nccf,_userType.distiller), Auth , editUser )
+userManagementRoutes.put('/toggleStatus/:id',authenticateUser,authorizeRoles(_userType.ho ,_userType.warehouse ,_userType.admin ,_userType.bo,_userType.nccf,_userType.distiller), Auth , toggleStatus )
 userManagementRoutes.get('/getUsersByUser', Auth , getUsersByUser)
-userManagementRoutes.get('/getSingleUser/:id',authenticateUser,authorizeRoles(_userType.admin), Auth , getSingleUser)
+userManagementRoutes.get('/getSingleUser/:id',authenticateUser,authorizeRoles(_userType.ho ,_userType.warehouse ,_userType.admin ,_userType.bo,_userType.nccf,_userType.distiller), Auth , getSingleUser)
 
 
 //portal routes 
